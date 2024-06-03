e93e2cd9abc55d7d879284d24f27c94effe30111
Robert Lamacraft <robert.lamacraft@researchspace.com>
2024-06-03 16:35:35 +0100

    Sidebar

---
+++ src/main/webapp/ui/src/eln/gallery/components/Sidebar.js
@@ -0,0 +1,241 @@
+//@flow
+
+import React, { type Node } from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import { styled } from "@mui/material/styles";
+import { COLOR } from "../theme";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import useViewportDimensions from "../../../util/useViewportDimensions";
+import { darken } from "@mui/system";
+import { gallerySectionLabel } from "../picker/common";
+import { FontAwesomeIcon as FaIcon } from "@fortawesome/react-fontawesome";
+import ChemistryIcon from "../picker/chemistryIcon";
+import Divider from "@mui/material/Divider";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faImage,
+  faFilm,
+  faFile,
+  faFileInvoice,
+  faDatabase,
+  faShapes,
+  faCircleDown,
+  faVolumeLow,
+} from "@fortawesome/free-solid-svg-icons";
+import { faNoteSticky } from "@fortawesome/free-regular-svg-icons";
+library.add(faImage);
+library.add(faFilm);
+library.add(faFile);
+library.add(faFileInvoice);
+library.add(faDatabase);
+library.add(faShapes);
+library.add(faNoteSticky);
+library.add(faCircleDown);
+library.add(faVolumeLow);
+
+const CustomDrawer = styled(Drawer)(({ open }) => ({
+  width: open ? "200px" : "64px",
+  transition: window.matchMedia("(prefers-reduced-motion: reduce)").matches
+    ? "none"
+    : "width .25s cubic-bezier(0.4, 0, 0.2, 1)",
+  "& .MuiPaper-root": {
+    position: "relative",
+    overflowX: "hidden",
+  },
+}));
+
+const SelectedDrawerTabIndicator = styled(({ className }) => (
+  <div className={className}></div>
+))(({ verticalPosition }) => ({
+  width: "198px",
+  height: "43px",
+  backgroundColor: window.matchMedia("(prefers-contrast: more)").matches
+    ? "black"
+    : `hsl(${COLOR.background.hue}deg, ${COLOR.background.saturation}%, ${COLOR.background.lightness}%)`,
+  position: "absolute",
+  top: verticalPosition,
+  transition: window.matchMedia("(prefers-reduced-motion: reduce)").matches
+    ? "none"
+    : "top 400ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
+}));
+
+const DrawerTab = styled(
+  ({ icon, label, index, className, selected, onClick }) => (
+    <ListItem disablePadding className={className}>
+      <ListItemButton selected={selected} onClick={onClick}>
+        <ListItemIcon>{icon}</ListItemIcon>
+        <ListItemText
+          primary={label}
+          sx={{ transitionDelay: `${(index + 1) * 0.02}s !important` }}
+        />
+      </ListItemButton>
+    </ListItem>
+  )
+)(({ drawerOpen }) => ({
+  position: "static",
+  "& .MuiListItemText-root": {
+    transition: window.matchMedia("(prefers-reduced-motion: reduce)").matches
+      ? "none"
+      : "all .2s cubic-bezier(0.4, 0, 0.2, 1)",
+    opacity: drawerOpen ? 1 : 0,
+    transform: drawerOpen ? "unset" : "translateX(-20px)",
+    textTransform: "uppercase",
+  },
+  "& .MuiListItemButton-root": {
+    "&:hover": {
+      backgroundColor: darken(
+        `hsl(${COLOR.background.hue}deg, ${COLOR.background.saturation}%, 100%)`,
+        0.05
+      ),
+    },
+    "&.Mui-selected": {
+      backgroundColor: "unset",
+      "&:hover": {
+        backgroundColor: "unset",
+      },
+    },
+  },
+}));
+
+export default function GallerySidebar({
+  selectedSection,
+  setSelectedSection,
+}: {|
+  selectedSection: string,
+  setSelectedSection: (string) => void,
+|}): Node {
+  const viewport = useViewportDimensions();
+  const [drawerOpen] = React.useState(!viewport.isViewportSmall);
+  const [selectedIndicatorOffset, setSelectedIndicatorOffset] =
+    React.useState(181);
+
+  return (
+    <CustomDrawer
+      open={drawerOpen}
+      anchor="left"
+      variant="permanent"
+      aria-label="gallery sections drawer"
+    >
+      <Box
+        sx={{
+          overflowY: "auto",
+          overflowX: "hidden",
+          position: "relative",
+        }}
+      >
+        <SelectedDrawerTabIndicator
+          verticalPosition={selectedIndicatorOffset}
+        />
+        <List sx={{ position: "static" }} role="navigation">
+          <DrawerTab
+            label={gallerySectionLabel.Images}
+            icon={<FaIcon icon="image" />}
+            index={0}
+            drawerOpen={drawerOpen}
+            selected={selectedSection === "Images"}
+            onClick={(event) => {
+              setSelectedSection("Images");
+              setSelectedIndicatorOffset(event.currentTarget.offsetTop);
+            }}
+          />
+          <DrawerTab
+            label={gallerySectionLabel.Audios}
+            icon={<FaIcon icon="volume-low" />}
+            index={1}
+            drawerOpen={drawerOpen}
+            selected={selectedSection === "Audios"}
+            onClick={(event) => {
+              setSelectedSection("Audios");
+              setSelectedIndicatorOffset(event.currentTarget.offsetTop);
+            }}
+          />
+          <DrawerTab
+            label={gallerySectionLabel.Videos}
+            icon={<FaIcon icon="film" />}
+            index={2}
+            drawerOpen={drawerOpen}
+            selected={selectedSection === "Videos"}
+            onClick={(event) => {
+              setSelectedSection("Videos");
+              setSelectedIndicatorOffset(event.currentTarget.offsetTop);
+            }}
+          />
+          <DrawerTab
+            label={gallerySectionLabel.Documents}
+            icon={<FaIcon icon="file" />}
+            index={3}
+            drawerOpen={drawerOpen}
+            selected={selectedSection === "Documents"}
+            onClick={(event) => {
+              setSelectedSection("Documents");
+              setSelectedIndicatorOffset(event.currentTarget.offsetTop);
+            }}
+          />
+          <DrawerTab
+            label={gallerySectionLabel.Chemistry}
+            icon={<ChemistryIcon />}
+            index={4}
+            drawerOpen={drawerOpen}
+            selected={selectedSection === "Chemistry"}
+            onClick={(event) => {
+              setSelectedSection("Chemistry");
+              setSelectedIndicatorOffset(event.currentTarget.offsetTop);
+            }}
+          />
+          <DrawerTab
+            label={gallerySectionLabel.DMPs}
+            icon={<FaIcon icon="file-invoice" />}
+            index={5}
+            drawerOpen={drawerOpen}
+            selected={selectedSection === "DMPs"}
+            onClick={(event) => {
+              setSelectedSection("DMPs");
+              setSelectedIndicatorOffset(event.currentTarget.offsetTop);
+            }}
+          />
+          <DrawerTab
+            label={gallerySectionLabel.Snippets}
+            icon={<FaIcon icon="fa-regular fa-note-sticky" />}
+            index={7}
+            drawerOpen={drawerOpen}
+            selected={selectedSection === "Snippets"}
+            onClick={(event) => {
+              setSelectedSection("Snippets");
+              setSelectedIndicatorOffset(event.currentTarget.offsetTop);
+            }}
+          />
+          <DrawerTab
+            label={gallerySectionLabel.Miscellaneous}
+            icon={<FaIcon icon="shapes" />}
+            index={8}
+            drawerOpen={drawerOpen}
+            selected={selectedSection === "Miscellaneous"}
+            onClick={(event) => {
+              setSelectedSection("Miscellaneous");
+              setSelectedIndicatorOffset(event.currentTarget.offsetTop);
+            }}
+          />
+        </List>
+        <Divider />
+        <List sx={{ position: "static" }} role="navigation">
+          <DrawerTab
+            label={gallerySectionLabel.PdfDocuments}
+            icon={<FaIcon icon="fa-circle-down" />}
+            index={9}
+            drawerOpen={drawerOpen}
+            selected={selectedSection === "PdfDocuments"}
+            onClick={(event) => {
+              setSelectedSection("PdfDocuments");
+              setSelectedIndicatorOffset(event.currentTarget.offsetTop);
+            }}
+          />
+        </List>
+      </Box>
+    </CustomDrawer>
+  );
+}
--- src/main/webapp/ui/src/eln/gallery/picker/index.js
+++ src/main/webapp/ui/src/eln/gallery/picker/index.js
@@ -12,13 +12,6 @@ import Button from "@mui/material/Button";
 import DialogActions from "@mui/material/DialogActions";
 import Box from "@mui/material/Box";
 import Breadcrumbs from "@mui/material/Breadcrumbs";
-import Drawer from "@mui/material/Drawer";
-import List from "@mui/material/List";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import { FontAwesomeIcon as FaIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {
   faImage,
@@ -31,15 +24,12 @@ import {
   faVolumeLow,
 } from "@fortawesome/free-solid-svg-icons";
 import { faNoteSticky } from "@fortawesome/free-regular-svg-icons";
-import Divider from "@mui/material/Divider";
 import Chip from "@mui/material/Chip";
 import createAccentedTheme from "../../../accentedTheme";
 import { COLORS as baseThemeColors } from "../../../theme";
-import ChemistryIcon from "./chemistryIcon";
 import Avatar from "@mui/material/Avatar";
 import Grow from "@mui/material/Grow";
 import useViewportDimensions from "../../../util/useViewportDimensions";
-import { darken } from "@mui/system";
 import useGalleryListing, { type GalleryFile } from "./useGalleryListing";
 import FileIcon from "@mui/icons-material/InsertDriveFile";
 import Fade from "@mui/material/Fade";
@@ -49,6 +39,7 @@ import { gallerySectionLabel } from "./common";
 import ValidatingSubmitButton from "../../../components/ValidatingSubmitButton";
 import { Result } from "../../../util/result";
 import { observer } from "mobx-react-lite";
+import Sidebar from "../components/Sidebar";
 library.add(faImage);
 library.add(faFilm);
 library.add(faFile);
@@ -134,70 +125,6 @@ const CustomDialog = styled(Dialog)(({ theme }) => ({
     overflowY: "unset",
     padding: theme.spacing(1.5, 2),
   },
-}));
-
-const CustomDrawer = styled(Drawer)(({ open }) => ({
-  width: open ? "200px" : "64px",
-  transition: window.matchMedia("(prefers-reduced-motion: reduce)").matches
-    ? "none"
-    : "width .25s cubic-bezier(0.4, 0, 0.2, 1)",
-  "& .MuiPaper-root": {
-    position: "relative",
-    overflowX: "hidden",
-  },
-}));
-
-const DrawerTab = styled(
-  ({ icon, label, index, className, selected, onClick }) => (
-    <ListItem disablePadding className={className}>
-      <ListItemButton selected={selected} onClick={onClick}>
-        <ListItemIcon>{icon}</ListItemIcon>
-        <ListItemText
-          primary={label}
-          sx={{ transitionDelay: `${(index + 1) * 0.02}s !important` }}
-        />
-      </ListItemButton>
-    </ListItem>
-  )
-)(({ drawerOpen }) => ({
-  position: "static",
-  "& .MuiListItemText-root": {
-    transition: window.matchMedia("(prefers-reduced-motion: reduce)").matches
-      ? "none"
-      : "all .2s cubic-bezier(0.4, 0, 0.2, 1)",
-    opacity: drawerOpen ? 1 : 0,
-    transform: drawerOpen ? "unset" : "translateX(-20px)",
-    textTransform: "uppercase",
-  },
-  "& .MuiListItemButton-root": {
-    "&:hover": {
-      backgroundColor: darken(
-        `hsl(${COLOR.background.hue}deg, ${COLOR.background.saturation}%, 100%)`,
-        0.05
-      ),
-    },
-    "&.Mui-selected": {
-      backgroundColor: "unset",
-      "&:hover": {
-        backgroundColor: "unset",
-      },
-    },
-  },
-}));
-
-const SelectedDrawerTabIndicator = styled(({ className }) => (
-  <div className={className}></div>
-))(({ verticalPosition }) => ({
-  width: "198px",
-  height: "43px",
-  backgroundColor: window.matchMedia("(prefers-contrast: more)").matches
-    ? "black"
-    : `hsl(${COLOR.background.hue}deg, ${COLOR.background.saturation}%, ${COLOR.background.lightness}%)`,
-  position: "absolute",
-  top: verticalPosition,
-  transition: window.matchMedia("(prefers-reduced-motion: reduce)").matches
-    ? "none"
-    : "top 400ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
 }));
 
 const FileCard = styled(
@@ -402,14 +329,11 @@ CustomGrow.displayName = "CustomGrow";
 const Picker = observer(
   ({ open, onClose }: {| open: boolean, onClose: () => void |}) => {
     const viewport = useViewportDimensions();
-    const [drawerOpen] = React.useState(!viewport.isViewportSmall);
-    const [selectedIndicatorOffset, setSelectedIndicatorOffset] =
-      React.useState(181);
-    const [selectedSection, setSelectedSection] = React.useState("Chemistry");
     const [selectedFile, setSelectedFile] = React.useState<GalleryFile | null>(
       null
     );
     const [appliedSearchTerm, setAppliedSearchTerm] = React.useState("");
+    const [selectedSection, setSelectedSection] = React.useState("Chemistry");
     const { galleryListing, path, clearPath } = useGalleryListing({
       section: selectedSection,
       searchTerm: appliedSearchTerm,
@@ -431,128 +355,10 @@ const Picker = observer(
           setAppliedSearchTerm={setAppliedSearchTerm}
         />
         <Box sx={{ display: "flex", height: "calc(100% - 48px)" }}>
-          <CustomDrawer
-            open={drawerOpen}
-            anchor="left"
-            variant="permanent"
-            aria-label="gallery sections drawer"
-          >
-            <Box
-              sx={{
-                overflowY: "auto",
-                overflowX: "hidden",
-                position: "relative",
-              }}
-            >
-              <SelectedDrawerTabIndicator
-                verticalPosition={selectedIndicatorOffset}
-              />
-              <List sx={{ position: "static" }} role="navigation">
-                <DrawerTab
-                  label={gallerySectionLabel.Images}
-                  icon={<FaIcon icon="image" />}
-                  index={0}
-                  drawerOpen={drawerOpen}
-                  selected={selectedSection === "Images"}
-                  onClick={(event) => {
-                    setSelectedSection("Images");
-                    setSelectedIndicatorOffset(event.currentTarget.offsetTop);
-                  }}
-                />
-                <DrawerTab
-                  label={gallerySectionLabel.Audios}
-                  icon={<FaIcon icon="volume-low" />}
-                  index={1}
-                  drawerOpen={drawerOpen}
-                  selected={selectedSection === "Audios"}
-                  onClick={(event) => {
-                    setSelectedSection("Audios");
-                    setSelectedIndicatorOffset(event.currentTarget.offsetTop);
-                  }}
-                />
-                <DrawerTab
-                  label={gallerySectionLabel.Videos}
-                  icon={<FaIcon icon="film" />}
-                  index={2}
-                  drawerOpen={drawerOpen}
-                  selected={selectedSection === "Videos"}
-                  onClick={(event) => {
-                    setSelectedSection("Videos");
-                    setSelectedIndicatorOffset(event.currentTarget.offsetTop);
-                  }}
-                />
-                <DrawerTab
-                  label={gallerySectionLabel.Documents}
-                  icon={<FaIcon icon="file" />}
-                  index={3}
-                  drawerOpen={drawerOpen}
-                  selected={selectedSection === "Documents"}
-                  onClick={(event) => {
-                    setSelectedSection("Documents");
-                    setSelectedIndicatorOffset(event.currentTarget.offsetTop);
-                  }}
-                />
-                <DrawerTab
-                  label={gallerySectionLabel.Chemistry}
-                  icon={<ChemistryIcon />}
-                  index={4}
-                  drawerOpen={drawerOpen}
-                  selected={selectedSection === "Chemistry"}
-                  onClick={(event) => {
-                    setSelectedSection("Chemistry");
-                    setSelectedIndicatorOffset(event.currentTarget.offsetTop);
-                  }}
-                />
-                <DrawerTab
-                  label={gallerySectionLabel.DMPs}
-                  icon={<FaIcon icon="file-invoice" />}
-                  index={5}
-                  drawerOpen={drawerOpen}
-                  selected={selectedSection === "DMPs"}
-                  onClick={(event) => {
-                    setSelectedSection("DMPs");
-                    setSelectedIndicatorOffset(event.currentTarget.offsetTop);
-                  }}
-                />
-                <DrawerTab
-                  label={gallerySectionLabel.Snippets}
-                  icon={<FaIcon icon="fa-regular fa-note-sticky" />}
-                  index={7}
-                  drawerOpen={drawerOpen}
-                  selected={selectedSection === "Snippets"}
-                  onClick={(event) => {
-                    setSelectedSection("Snippets");
-                    setSelectedIndicatorOffset(event.currentTarget.offsetTop);
-                  }}
-                />
-                <DrawerTab
-                  label={gallerySectionLabel.Miscellaneous}
-                  icon={<FaIcon icon="shapes" />}
-                  index={8}
-                  drawerOpen={drawerOpen}
-                  selected={selectedSection === "Miscellaneous"}
-                  onClick={(event) => {
-                    setSelectedSection("Miscellaneous");
-                    setSelectedIndicatorOffset(event.currentTarget.offsetTop);
-                  }}
-                />
-              </List>
-              <Divider />
-              <List sx={{ position: "static" }} role="navigation">
-                <DrawerTab
-                  label={gallerySectionLabel.PdfDocuments}
-                  icon={<FaIcon icon="fa-circle-down" />}
-                  index={9}
-                  drawerOpen={drawerOpen}
-                  selected={selectedSection === "PdfDocuments"}
-                  onClick={(event) => {
-                    setSelectedSection("PdfDocuments");
-                    setSelectedIndicatorOffset(event.currentTarget.offsetTop);
-                  }}
-                />
-              </List>
-            </Box>
-          </CustomDrawer>
+          <Sidebar
+            selectedSection={selectedSection}
+            setSelectedSection={setSelectedSection}
+          />
           <Box
             sx={{
               height: "100%",
