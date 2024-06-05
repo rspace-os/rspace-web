9b750827ff431d42820d9c69b91388ead54a0490
Robert Lamacraft <robert.lamacraft@researchspace.com>
2024-06-05 10:13:15 +0100

    Upload a single file

--- src/main/webapp/ui/src/eln/gallery/useGalleryActions.js
+++ src/main/webapp/ui/src/eln/gallery/useGalleryActions.js
@@ -4,10 +4,25 @@ import axios from "axios";
 import * as FetchingData from "../../util/fetchingData";
 
 export default function useGalleryActions(): {|
-  uploadFiles: ($ReadOnlyArray<File>) => void,
+  uploadFiles: ($ReadOnlyArray<File>) => Promise<void>,
 |} {
   function uploadFiles(files: $ReadOnlyArray<File>) {
     console.debug(files);
+
+    // TODO open "uploading" alert
+    const formData = new FormData();
+    formData.append("xfile", files[0]);
+    // TODO pass folderId in as a parameter
+    formData.append("targetFolderId", 8);
+    // TODO upload each file in parallel
+    return axios.post("gallery/ajax/uploadFile", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    // TODO close "uploading" alert
+    // TODO if error, show error alert
+    // TODO if success, show success alert
   }
 
   return { uploadFiles };
