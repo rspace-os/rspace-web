065ea0fc0651482010aaefd9b993f3795a88115d
Robert Lamacraft <robert.lamacraft@researchspace.com>
2024-06-19 17:44:23 +0100

    More documentation

--- src/main/webapp/ui/src/components/useVerticalRovingTabIndex.js
+++ src/main/webapp/ui/src/components/useVerticalRovingTabIndex.js
@@ -3,6 +3,34 @@
 import React from "react";
 import { modulo } from "../util/Util";
 
+/**
+ * A roving tab index is a pattern used to allow a keyboard-centric user to
+ * more efficiently move around the web page by grouping particular controls
+ * together, such that tab and shift-tab enter and leave the group and the
+ * arrow keys are used to move between the elements. As such, pressing tab
+ * twice will skip over all of the elements in the group, which is much faster
+ * than tabbing through them all.
+ *
+ * This custom hook provides an abstraction over implementing a roving tab
+ * index for a vertical list of elements, so that the up and down arrow keys
+ * provide navigation between the elements in the list. Tab and shift-tab move
+ * the user's focus out of the list, and when they return the focus resumes on
+ * the last element in the list that had focus.
+ *
+ * There are two parts to how this custom hook is to be used:
+ *
+ *   1. Attach the `eventHandlers` to the component that wraps all of the
+ *      elements in the list. It is this that captures the keyboard events.
+ *
+ *   2. Pass a `tabIndex` and a `ref` to each element in the list by calling
+ *      the `getTabIndex` and `getRef` functions respectively with each
+ *      element's index.
+ *
+ * For more information on roving tab index, see these sites:
+ *  - https://web.dev/articles/control-focus-with-tabindex#use_roving_tabindex
+ *  - https://www.w3.org/WAI/ARIA/apg/patterns/radio/examples/radio/
+ *  - https://www.youtube.com/watch?v=uCIC2LNt0bk
+ */
 export default function useVerticalRovingTabIndex<RefComponent: HTMLElement>({
   max,
 }: {|
@@ -12,6 +40,15 @@ export default function useVerticalRovingTabIndex<RefComponent: HTMLElement>({
    */
   max: number,
 |}): {|
+  /**
+   * The set of the event handlers that must be attached to the container
+   * component of all of the elements of the vertical list.
+   */
+  eventHandlers: {|
+    onFocus: () => void,
+    onBlur: () => void,
+    onKeyDown: (KeyboardEvent) => void,
+  |},
   /**
    * Given the index of an element of the vertical list, return that element's
    * current tab index.
@@ -23,16 +60,6 @@ export default function useVerticalRovingTabIndex<RefComponent: HTMLElement>({
    * a tab index of 0. Otherwise returns null.
    */
   getRef: (number) => null | {| current: RefComponent | null |},
-
-  /**
-   * The set of the event handlers that must be attached to the container
-   * component of all of the elements of the vertical list.
-   */
-  eventHandlers: {|
-    onFocus: () => void,
-    onBlur: () => void,
-    onKeyDown: (KeyboardEvent) => void,
-  |},
 |} {
   const [rovingTabIndex, setRovingTabIndex] = React.useState(0);
   const [hasFocus, setHasFocus] = React.useState(false);
@@ -61,6 +88,10 @@ export default function useVerticalRovingTabIndex<RefComponent: HTMLElement>({
   }
 
   function onKeyDown(e: KeyboardEvent) {
+    /*
+     * By using modulo rather than min and max the user's focus wraps around
+     * when reaching the end.
+     */
     if (e.key === "ArrowUp") {
       setRovingTabIndex(modulo(rovingTabIndex - 1, max + 1));
     } else if (e.key === "ArrowDown") {
