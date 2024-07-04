1b124097eb99dec4b9ea4aa8b30aeeb074f865b5
Leonardo Lenoci <l.lenoci@science.leidenuniv.nl>
2024-07-04 10:36:32 +0200

    Implemented reviewers suggestions by @rs-nicof

--- src/main/java/com/researchspace/netfiles/NfsFactory.java
+++ src/main/java/com/researchspace/netfiles/NfsFactory.java
@@ -22,6 +22,8 @@ import org.springframework.stereotype.Component;
 /** Class providing various Net File Store implementations depending on deployment.properties */
 @Component
 public class NfsFactory {
+    
+  private static final int IRODS_DEFAULT_PORT = 1247;
 
   private static final Logger log = LoggerFactory.getLogger(NfsFactory.class);
 
@@ -90,11 +92,8 @@ public class NfsFactory {
     }
     if (NfsClientType.IRODS.equals(clientType)) {
       int irodsPort;
-      if (StringUtils.isEmpty(fileSystem.getClientOption(NfsFileSystemOption.IRODS_PORT))) {
-        irodsPort = 1247;
-      } else {
-        irodsPort = Integer.parseInt(fileSystem.getClientOption(NfsFileSystemOption.IRODS_PORT));
-      }
+      irodsPort = (StringUtils.isBlank(fileSystem.getClientOption(NfsFileSystemOption.IRODS_PORT))) ? IRODS_DEFAULT_PORT : Integer.parseInt(fileSystem.getClientOption(NfsFileSystemOption.IRODS_PORT));
+
       return new IRODSClient(
           new IRODSAccount(
               fileSystem.getUrl(),
