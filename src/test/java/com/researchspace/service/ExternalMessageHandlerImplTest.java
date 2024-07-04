9111977aae11993b344f07f35c1d843c3f2fae51
matthias <matthias@researchspace.com>
2024-07-04 15:48:40 +0100

    rewrite real-connection slack unit tests to use webhook url from deployment.properties

--- src/test/java/com/researchspace/service/ExternalMessageHandlerImplTest.java
+++ src/test/java/com/researchspace/service/ExternalMessageHandlerImplTest.java
@@ -36,18 +36,20 @@ public class ExternalMessageHandlerImplTest extends SpringTransactionalTest {
 
   private static final String EXPECTED_MESSAGE = "hello from Spring Test";
 
-  User u1 = null;
-
-  private @Autowired ExternalMessageHandler handler;
-  private @Autowired UserAppConfigManager mgr;
+  @Value("${slack.realConnectionTest.webhookUrl}")
+  private String slackTestWebhookUrl;
 
   @Value("${msteams.realConnectionTest.webhookUrl}")
   private String msTeamsTestWebhookUrl;
 
+  private @Autowired ExternalMessageHandler handler;
+  private @Autowired UserAppConfigManager mgr;
+  private User testUser;
+
   @Before
   public void setUp() throws Exception {
-    u1 = createAndSaveRandomUser();
-    initialiseContentWithEmptyContent(u1);
+    testUser = createAndSaveRandomUser();
+    initialiseContentWithEmptyContent(testUser);
   }
 
   @After
@@ -56,37 +58,37 @@ public class ExternalMessageHandlerImplTest extends SpringTransactionalTest {
   @Test
   @RunIfSystemPropertyDefined(value = "nightly")
   public void sendExternalMessageToSlack() {
-    StructuredDocument doc = createBasicDocumentInRootFolderWithText(u1, "any");
-    StructuredDocument doc2 = createBasicDocumentInRootFolderWithText(u1, "any2");
-    StructuredDocument doc3 = createBasicDocumentInRootFolderWithText(u1, "any3");
-    Long cfgSetId = setUpAppConfigForUser(u1, () -> getSlackDevDfg());
-    logoutAndLoginAs(u1);
+    StructuredDocument doc = createBasicDocumentInRootFolderWithText(testUser, "any");
+    StructuredDocument doc2 = createBasicDocumentInRootFolderWithText(testUser, "any2");
+    StructuredDocument doc3 = createBasicDocumentInRootFolderWithText(testUser, "any3");
+    Long cfgSetId = setUpAppConfigForUser(testUser, () -> getSlackDevDfg());
+    logoutAndLoginAs(testUser);
 
     ServiceOperationResult<ResponseEntity<String>> response =
         handler.sendExternalMessage(
-            EXPECTED_MESSAGE, cfgSetId, toList(doc.getId(), doc2.getId(), doc3.getId()), u1);
+            EXPECTED_MESSAGE, cfgSetId, toList(doc.getId(), doc2.getId(), doc3.getId()), testUser);
     assertTrue(response.isSucceeded());
     ServiceOperationResult<ResponseEntity<String>> response2 =
-        handler.sendExternalMessage(EXPECTED_MESSAGE, cfgSetId, toList(doc.getId()), u1);
+        handler.sendExternalMessage(EXPECTED_MESSAGE, cfgSetId, toList(doc.getId()), testUser);
     assertTrue(response2.isSucceeded());
   }
 
   @Test
   @RunIfSystemPropertyDefined(value = "nightly")
   public void sendExternalMessageToMSTeams() {
-    StructuredDocument doc = createBasicDocumentInRootFolderWithText(u1, "any");
-    StructuredDocument doc2 = createBasicDocumentInRootFolderWithText(u1, "any2");
-    StructuredDocument doc3 = createBasicDocumentInRootFolderWithText(u1, "any3");
-    Long cfgSetId = setUpAppConfigForUser(u1, () -> getMsTeamsCfg());
-    logoutAndLoginAs(u1);
+    StructuredDocument doc = createBasicDocumentInRootFolderWithText(testUser, "any");
+    StructuredDocument doc2 = createBasicDocumentInRootFolderWithText(testUser, "any2");
+    StructuredDocument doc3 = createBasicDocumentInRootFolderWithText(testUser, "any3");
+    Long cfgSetId = setUpAppConfigForUser(testUser, () -> getMsTeamsCfg());
+    logoutAndLoginAs(testUser);
 
     ServiceOperationResult<ResponseEntity<String>> response =
         handler.sendExternalMessage(
-            EXPECTED_MESSAGE, cfgSetId, toList(doc.getId(), doc2.getId(), doc3.getId()), u1);
+            EXPECTED_MESSAGE, cfgSetId, toList(doc.getId(), doc2.getId(), doc3.getId()), testUser);
     assertTrue(response.isSucceeded());
 
     ServiceOperationResult<ResponseEntity<String>> response2 =
-        handler.sendExternalMessage(EXPECTED_MESSAGE, cfgSetId, toList(doc.getId()), u1);
+        handler.sendExternalMessage(EXPECTED_MESSAGE, cfgSetId, toList(doc.getId()), testUser);
     assertTrue(response2.isSucceeded());
   }
 
@@ -104,7 +106,7 @@ public class ExternalMessageHandlerImplTest extends SpringTransactionalTest {
   private Long setUpNonMessageAppConfig() {
     Map<String, String> config = new HashMap<>();
     config.put("otherAppConfig", "some value");
-    UserAppConfig appConfig = mgr.saveAppConfigElementSet(config, null, false, u1);
+    UserAppConfig appConfig = mgr.saveAppConfigElementSet(config, null, false, testUser);
     Long cfgSetId = appConfig.getAppConfigElementSets().iterator().next().getId();
     return cfgSetId;
   }
@@ -113,23 +115,23 @@ public class ExternalMessageHandlerImplTest extends SpringTransactionalTest {
   public void sendExternalMessageReturnsEmptyForNonMessageApp() {
 
     Long cfgSetId = setUpNonMessageAppConfig();
-    logoutAndLoginAs(u1);
+    logoutAndLoginAs(testUser);
     ServiceOperationResult<ResponseEntity<String>> response =
-        handler.sendExternalMessage(EXPECTED_MESSAGE, cfgSetId, toList(-10L), u1);
+        handler.sendExternalMessage(EXPECTED_MESSAGE, cfgSetId, toList(-10L), testUser);
     assertFalse(response.isSucceeded());
   }
 
   @Test
   public void sendExternalMessageReturnsEmptyForUnknownId() {
     ServiceOperationResult<ResponseEntity<String>> response =
-        handler.sendExternalMessage(EXPECTED_MESSAGE, -100L, toList(-10L), u1);
+        handler.sendExternalMessage(EXPECTED_MESSAGE, -100L, toList(-10L), testUser);
     assertFalse(response.isSucceeded());
   }
 
   @Test()
   public void sendExternalMessagePermissions() throws Exception {
-    Long cfgSetId = setUpAppConfigForUser(u1, () -> getSlackDevDfg());
-    logoutAndLoginAs(u1);
+    Long cfgSetId = setUpAppConfigForUser(testUser, () -> getSlackDevDfg());
+    logoutAndLoginAs(testUser);
     // other user can't access u1's apps
     User u2 = createAndSaveRandomUser();
     initialiseContentWithEmptyContent(u2);
@@ -142,7 +144,7 @@ public class ExternalMessageHandlerImplTest extends SpringTransactionalTest {
     Map<String, String> map = new HashMap<>();
     map.put("SLACK_CHANNEL_NAME", "general");
     map.put("SLACK_TEAM_NAME", "rspacedev");
-    map.put("SLACK_WEBHOOK_URL", SlackPosterTest.SLACK_WEBHOOK);
+    map.put("SLACK_WEBHOOK_URL", slackTestWebhookUrl);
     map.put("SLACK_CHANNEL_LABEL", "general");
     map.put("SLACK_USER_ID", "U123");
     map.put("SLACK_TEAM_ID", "T456");
@@ -157,4 +159,5 @@ public class ExternalMessageHandlerImplTest extends SpringTransactionalTest {
     map.put("MSTEAMS_WEBHOOK_URL", msTeamsTestWebhookUrl);
     return map;
   }
+
 }
