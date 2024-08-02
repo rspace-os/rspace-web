240b34b21f07415108c80e5320e23a1a4c78ed18
Robert Lamacraft <robert.lamacraft@researchspace.com>
2024-08-02 11:00:31 +0100

    RSDEV-311 Make Inventory permalink parsing more robust
    If the user accidentally enters an invalid value as an Id in a permalink they
    are currently getting an incomprehensible Java error. With this change, we
    attempt to correct their mistake if they enter a Global Id in place of an Id,
    or else we redirect to the root of the application if a more helpful error
    message.

--- src/main/webapp/ui/src/Inventory/PermalinkRouter.js
+++ src/main/webapp/ui/src/Inventory/PermalinkRouter.js
@@ -2,10 +2,14 @@
 
 import SearchRouter from "./Search/SearchRouter";
 import { observer } from "mobx-react-lite";
-import { useParams } from "react-router-dom";
+import { useParams, Navigate } from "react-router-dom";
 import React, { type Node, type ComponentType, useContext } from "react";
 import { type PermalinkType } from "../stores/definitions/Search";
 import NavigateContext, { type UseLocation } from "../stores/contexts/Navigate";
+import AlertContext, { mkAlert } from "../stores/contexts/Alert";
+import { inventoryRecordTypeLabels } from "../stores/definitions/InventoryRecord";
+import { globalIdPatterns } from "../stores/definitions/BaseRecord";
+import { match } from "../util/Util";
 
 type PermalinkRouterArgs = {|
   type: PermalinkType,
@@ -17,10 +21,52 @@ function PermalinkRouter({ type }: PermalinkRouterArgs): Node {
   const urlSearchParams = new URLSearchParams(
     (useLocation(): UseLocation).search
   );
+  const { addAlert } = useContext(AlertContext);
+
+  if (!id) return <Navigate to="/inventory" replace={true} />;
+
   let version = null;
   if (urlSearchParams.has("version")) {
-    version = parseInt(urlSearchParams.get("version"));
+    version = parseInt(urlSearchParams.get("version"), 10);
     if (isNaN(version)) return <h1>Invalid version parameter</h1>;
+  }
+
+  if (isNaN(parseInt(id, 10))) {
+    const recordType = match<
+      PermalinkType,
+      $Keys<typeof inventoryRecordTypeLabels> & $Keys<typeof globalIdPatterns>
+    >([
+      [(t) => t === "sample", "sample"],
+      [(t) => t === "container", "container"],
+      [(t) => t === "subsample", "subsample"],
+      [(t) => t === "sampletemplate", "sampleTemplate"],
+    ])(type);
+    /*
+     * If the user has provided the Global Id where they are meant to provide
+     * an Id, and it matches the passed `type` then we can correct their
+     * mistake by dropping the type prefix from the Global Id and silently
+     * redirecting.
+     */
+    if (globalIdPatterns[recordType].test(id)) {
+      return (
+        <Navigate
+          to={`/inventory/${type}/${id.slice(2)}?${urlSearchParams.toString()}`}
+          replace={true}
+        />
+      );
+    }
+    /*
+     * If the provided `id` is otherwise invalid then there is no clever
+     * recovery we can do but redirect to the root of the application and
+     * display an error.
+     */
+    addAlert(
+      mkAlert({
+        message: `"${id}" is not a valid ${inventoryRecordTypeLabels[recordType]} id.`,
+        variant: "error",
+      })
+    );
+    return <Navigate to="/inventory" replace={true} />;
   }
 
   return (
