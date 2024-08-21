e51802ad58fc26dcd58f1a12dd7185e7b9312167
Robert Lamacraft <robert.lamacraft@researchspace.com>
2024-08-21 16:35:56 +0100

    !flow Support storing multiple

--- src/main/webapp/ui/src/util/useUiPreference.js
+++ src/main/webapp/ui/src/util/useUiPreference.js
@@ -11,6 +11,12 @@ export const PREFERENCES: { [Preferences]: symbol } = {
   GALLERY_VIEW_MODE: Symbol.for("GALLERY_VIEW_MODE"),
 };
 
+function fetchPreferences(): Promise<{ ... }> {
+  return axios
+    .get<{ ... }>("/userform/ajax/preference?preference=UI_JSON_SETTINGS")
+    .then(({ data }) => data);
+}
+
 export default function useUiPreference(
   preference: $Values<typeof PREFERENCES>,
   opts: {|
@@ -20,12 +26,10 @@ export default function useUiPreference(
   const [value, setValue] = React.useState(opts.defaultValue);
 
   React.useEffect(() => {
-    void axios
-      .get("/userform/ajax/preference?preference=UI_JSON_SETTINGS")
-      .then(({ data }) => {
-        if (Symbol.keyFor(preference) in data)
-          setValue(data[Symbol.keyFor(preference)]);
-      });
+    void fetchPreferences().then((data) => {
+      if (Symbol.keyFor(preference) in data)
+        setValue(data[Symbol.keyFor(preference)]);
+    });
   }, []);
 
   return [
@@ -33,19 +37,27 @@ export default function useUiPreference(
     (newValue) => {
       setValue(newValue);
 
-      const formData = new FormData();
-      formData.append("preference", "UI_JSON_SETTINGS");
-      formData.append(
-        "value",
-        JSON.stringify({
-          [Symbol.keyFor(preference)]: newValue,
-        })
-      );
-      void axios.post("/userform/ajax/preference", formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      });
+      void (async () => {
+        const preferences = await fetchPreferences();
+        const formData = new FormData();
+        formData.append("preference", "UI_JSON_SETTINGS");
+        formData.append(
+          "value",
+          JSON.stringify({
+            ...preferences,
+            [Symbol.keyFor(preference)]: newValue,
+          })
+        );
+        await axios.post<FormData, mixed>(
+          "/userform/ajax/preference",
+          formData,
+          {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+          }
+        );
+      })();
     },
   ];
 }
