663c6443ba1b69a07a3c0a3aa5fb9867be3d52b4
Robert Lamacraft <robert.lamacraft@researchspace.com>
2024-08-29 10:52:06 +0100

    PRT-795 Add a unit test checking deletion of containers
    This is a unit test that would have caught the first issue fixed by commit
    80688bc858, which is to say the issue that wasn't itself caught by the unit
    tested added by that change.

--- src/main/webapp/ui/src/stores/models/__tests__/Search/deleteRecords.test.js
+++ src/main/webapp/ui/src/stores/models/__tests__/Search/deleteRecords.test.js
@@ -9,6 +9,11 @@ import Search from "../../Search";
 import InvApiService from "../../../../common/InvApiService";
 import { mockFactory } from "../../../definitions/__tests__/Factory/mocking";
 import { type InventoryRecord } from "../../../definitions/InventoryRecord";
+import { mkAlert } from "../../../contexts/Alert";
+
+jest.mock("../../../contexts/Alert", () => ({
+  mkAlert: jest.fn(() => ({})),
+}));
 
 jest.mock("../../../../common/InvApiService", () => ({
   bulk: jest.fn(),
@@ -53,6 +58,60 @@ describe("action: deleteRecords", () => {
        * be used again
        */
       expect(searchSpy).toHaveBeenCalledWith(null, expect.any(Function));
+    });
+    test("and there is an error, there should be a helpful error message.", async () => {
+      jest.spyOn(InvApiService, "bulk").mockImplementation(() =>
+        Promise.resolve({
+          data: {
+            results: [
+              {
+                record: null,
+                error: {
+                  status: "BAD_REQUEST",
+                  httpCode: 400,
+                  internalCode: 40002,
+                  message: "Errors detected : 1",
+                  messageCode: null,
+                  errors: ["container.deletion.failure.not.empty"],
+                  iso8601Timestamp: "2024-08-29T08:57:41.817794Z",
+                  data: null,
+                },
+              },
+            ],
+            successCount: 0,
+            successCountBeforeFirstError: 0,
+            errorCount: 1,
+            status: "COMPLETED",
+          },
+        })
+      );
+
+      const factory = mockFactory({
+        newFactory: () =>
+          mockFactory({
+            newRecord: jest
+              .fn<[any], InventoryRecord>()
+              .mockImplementation((attrs) => makeMockContainer(attrs)),
+          }),
+      });
+
+      const search = new Search({ factory });
+
+      jest
+        .spyOn(search.fetcher, "search")
+        .mockImplementation(() => Promise.resolve());
+
+      await search.deleteRecords([makeMockContainer()]);
+
+      expect(mkAlert).toHaveBeenCalledWith(
+        expect.objectContaining({
+          details: expect.arrayContaining([
+            expect.objectContaining({
+              title: "The container 'A list container' is not empty.",
+            }),
+          ]),
+        })
+      );
     });
   });
 });
