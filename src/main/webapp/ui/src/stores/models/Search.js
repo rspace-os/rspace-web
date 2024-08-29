rspace-os/rspace-web

commit 80688bc8582df9687f28db6be857d80a6acbd1ae

PRT-795 Fix error message when deleting container with contents
There are two bugs here

  1. A more recent one wherein the changes to deleteRecords that provided a
     richer user experience when deleting samples with only one subsample
     resulted in errors about containers that could not be deleted from being
     incorrectly parsed from the response and thus displaying an error.

  2. An older issue that dates back to the beginning of 2024, where error
     messages from the server that are period-separated words were not being
     correctly converted into user-friendly error messages because
     traverseObjectTree had a bug.

src/main/webapp/ui/src/stores/models/Search.js
@@ -464,17 +464,18 @@ export default class Search implements SearchInterface {
        */
       const samplesThatCouldNotBeDeleted = data.results
         .map(({ record }) => record)
+        .filter((r) => Boolean(r))
         .filter(({ type, canBeDeleted }) => type === "SAMPLE" && !canBeDeleted);
       const samplesThatCouldBeDeleted = data.results
         .map(({ record }) => record)
+        .filter((r) => Boolean(r))
         .filter(({ type, canBeDeleted }) => type === "SAMPLE" && canBeDeleted);
 
       const factory = this.factory.newFactory();
       const successfullyDeleted = [
         ...data.results
-          .filter(({ record: { type } }) => type !== "SAMPLE")
           .filter(({ error }) => !error)
-          .map(({ record }) => record),
+          .filter(({ record: { type } }) => type !== "SAMPLE"),
         ...samplesThatCouldBeDeleted,
       ].map((record) => {
         const newRecord = factory.newRecord(record);

src/main/webapp/ui/src/util/__tests__/unsafeUtils/traverseObjectTree.test.js
@@ -1,0 +1,25 @@
+/*
+ * @jest-environment jsdom
+ */
+//@flow
+/* eslint-env jest */
+
+import { traverseObjectTree } from "../../unsafeUtils";
+
+describe("traverseObjectTree", () => {
+  test("basic example", () => {
+    expect(
+      traverseObjectTree(
+        {
+          foo: {
+            bar: {
+              baz: "test",
+            },
+          },
+        },
+        "foo.bar.baz",
+        null
+      )
+    ).toBe("test");
+  });
+});

src/main/webapp/ui/src/util/unsafeUtils.js
@@ -23,9 +23,9 @@ export const traverseObjectTree = (
   const path = prop.split(".");
   for (let i = 0; i < path.length; i++) {
     // $FlowExpectedError[invalid-computed-prop]
-    if (typeof obj[path[i]] === "undefined") return defval;
+    if (typeof o[path[i]] === "undefined") return defval;
     // $FlowExpectedError[invalid-computed-prop]
     o = o[path[i]];
   }
-  return obj;
+  return o;
 };

commit 5380c9c3b31e355d49bbe044f9d071ad66107b46

Fix destructuring results from deleting Inventory records
This is a fix for commit 80688bc858, which accidentally removed the
destructuring of the inner `record` object. This was not caught by Flow because
`factory.newRecord` and `populateFromJson` are any-typed, it was however
thankfully caught by a unit test.

src/main/webapp/ui/src/stores/models/Search.js
@@ -473,7 +473,8 @@ export default class Search implements SearchInterface {
       const successfullyDeleted = [
         ...data.results
           .filter(({ error }) => !error)
-          .filter(({ record }) => record !== null && record.type !== "SAMPLE"),
+          .filter(({ record }) => record !== null && record.type !== "SAMPLE")
+          .map(({ record }) => record),
         ...samplesThatCouldBeDeleted,
       ].map((record) => {
         const newRecord = factory.newRecord(record);