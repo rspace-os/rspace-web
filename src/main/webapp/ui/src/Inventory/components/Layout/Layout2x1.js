d6c0813c5d62433364c0464f8bd589b369612d53
Robert Lamacraft <robert.lamacraft@researchspace.com>
2024-09-04 15:39:50 +0100

    Documentation

--- src/main/webapp/ui/src/Inventory/components/Layout/Layout2x1.js
+++ src/main/webapp/ui/src/Inventory/components/Layout/Layout2x1.js
@@ -9,11 +9,37 @@ import ExpandCollapseIcon from "../../../components/ExpandCollapseIcon";
 import IconButtonWithTooltip from "../../../components/IconButtonWithTooltip";
 import useViewportDimensions from "../../../util/useViewportDimensions";
 
+/**
+ * The main Inventory UI is divided into two column on large viewports and a
+ * single column on smaller viewports, such as mobile devices. This module
+ * implements the logic that determines how these two panels are rendered.
+ */
+
+/**
+ * When using a device with a wide viewport, the user has the option to hide
+ * the right panel so that they can view the table of search results with
+ * sufficient room to compare several columns of data. This context wraps the
+ * whole component tree inside of the Layout2x1 component (the default export
+ * of this module) but is not exported and thus is hidden from all those
+ * components.
+ */
 const UserHiddenRightPanelContext = React.createContext<{|
   userHiddenRightPanel: boolean,
   setUserHiddenRightPanel: (boolean) => void,
 |}>({ userHiddenRightPanel: false, setUserHiddenRightPanel: () => {} });
 
+/**
+ * If the UI is to be rendered with a single column -- either because the
+ * browser's viewport isn't sufficiently wide enough or because they have opted
+ * to hide the right panel on wider viewports -- then this custom hook returns
+ * true.
+ *
+ * It is through this hook that the all of the components within the layout can
+ * access the state managed by the UserHiddenRightPanelContext. All of these
+ * components shouldn't care whether the UI is in single column mode because of
+ * a small viewport or because the user has opted to hide the right panel, as
+ * they can only determine whether the value is true or false; not why.
+ */
 export function useIsSingleColumnLayout(): boolean {
   const viewportDimensions = useViewportDimensions();
   const { userHiddenRightPanel } = React.useContext(
@@ -26,6 +52,14 @@ export function useIsSingleColumnLayout(): boolean {
   );
 }
 
+/**
+ * This is the toggle for hiding the right panel. It is the only the component
+ * that cares whether the single column mode is due to an insufficiently wide
+ * viewport or because the user has opted to hide the right panel by tapping
+ * it. As such, it is included as an additional export in this module, allowing
+ * it to access the UserHiddenRightPanelContext without requiring that it be
+ * exported to the whole codebase.
+ */
 export const RightPanelToggle: ComponentType<{||}> = observer(() => {
   const { userHiddenRightPanel, setUserHiddenRightPanel } = React.useContext(
     UserHiddenRightPanelContext
@@ -95,7 +129,6 @@ type Layout2x1Args = {|
 /*
  * Defines a responsive layout, where both panels are shown on larger devices,
  * but one panel is hidden on smaller ones (depending on viewport size).
- * This logic is determined based on the state of uiStore.
  */
 const Layout2x1 = observer((props: Layout2x1Args) => {
   const { uiStore } = useStores();
