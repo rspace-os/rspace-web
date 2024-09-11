7bc68ec3e01466509d6fdb707aaa6a3ca0b9f246
Robert Lamacraft <robert.lamacraft@researchspace.com>
2024-09-11 17:37:42 +0100

    Documenting CallableAsposePreview

--- src/main/webapp/ui/src/eln/gallery/components/CallableAsposePreview.js
+++ src/main/webapp/ui/src/eln/gallery/components/CallableAsposePreview.js
@@ -7,9 +7,30 @@ import axios from "axios";
 import AlertContext, { mkAlert } from "../../../stores/contexts/Alert";
 import * as Parsers from "../../../util/parsers";
 
+/*
+ * If aspose is configured, then users can preview the contents of various
+ * common document file types by running them through the PDF generator and
+ * viewing that PDF via CallablePdfPreview.
+ *
+ * Much like how `window.open` allows any JS code on the page to trigger the
+ * opening of in a new window/tab with a specified URL, this module provides a
+ * mechanism to allow any JS code to trigger the opening of a dialog by
+ * providing a GalleryFile that can be converted to a PDF by Aspose.
+ *
+ * If the conversion fails or there is an error for any other reason with the
+ * network call then an error toast is shown.
+ */
+
 const AsposePreviewContext = React.createContext((_file: GalleryFile) => {});
 
+/**
+ * Use the callable aspose preview component to display a document in a dialog
+ * as a PDF.
+ */
 export function useAsposePreview(): {|
+  /**
+   * Preview the document at this GalleryFile.
+   */
   openAsposePreview: (GalleryFile) => void,
 |} {
   const openAsposePreview = React.useContext(AsposePreviewContext);
@@ -19,6 +40,12 @@ export function useAsposePreview(): {|
 }
 
 /**
+ * This components provides a mechanism for any other component that is its
+ * descendent to trigger the previewing of a document by passing the GalleryFile
+ * to a call to `useAsposePreview`'s `openAsposePreview`. Just do something like
+ *    const { openAsposePreview } = useImagePreview();
+ *    openAsposePreview("http://example.com/document.doc");
+ *
  * Relies on being inside of an AlertContext and the context created by
  * CallablePdfPreview
  */
