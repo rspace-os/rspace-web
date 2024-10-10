470f2bd433d17a2041b3988c744c312d726b66d9
Robert Lamacraft <robert.lamacraft@researchspace.com>
2024-10-10 16:02:44 +0100

    fixup! Fix some tests caused by use of unit store

--- src/main/webapp/ui/src/stores/models/__tests__/SubSampleModel/permalinkURL.test.js
+++ src/main/webapp/ui/src/stores/models/__tests__/SubSampleModel/permalinkURL.test.js
@@ -7,7 +7,11 @@ import "@testing-library/jest-dom";
 import { makeMockSubSample } from "./mocking";
 
 jest.mock("../../../../common/InvApiService", () => {});
-jest.mock("../../../../stores/stores/RootStore", () => () => ({}));
+jest.mock("../../../../stores/stores/RootStore", () => () => ({
+  unitStore: {
+    getUnit: () => ({ label: "ml" }),
+  },
+}));
 
 describe("permalinkURL", () => {
   test("When the subsample has not yet been saved, the permalinkURL should be null.", () => {
