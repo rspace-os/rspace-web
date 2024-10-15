rspace-os/rspace-web

commit c65e40c6c9b8f5e8951af0891bd447cb86863089

PRT-791 Add error handling to fetching tags in export dialog
This change improves the error handling of calls to `/exportRecordTagsArchive`
in two ways.

First, a `.catch` clause is add to the promise so that should the response code
be anything but a 200 then the Add Tag button will not become inoperable, but
instead simply show no auto-suggested tags. By itself, that would actually
solve this bug as although the server is responding with a 200 with a payload
that describes an error when attempting to get the tags associated with a
Gallery file, attempting to use that error object as an array of string will
fail when we do `data.flatMap`.

However, this approach to the catching of errors that come in a 200 response is
not especially obvious. Instead, this change also incorporates richer parsing
logic for decoding the payload associated with the response. Instead of
assuming that the response will always be an array of strings, the response
object is parsed and should it not fit this shape (as it will not when fetching
the tags of a Gallery file) then again an empty array of tags is returned and
the "Add Tag" button remains operable.

src/main/webapp/ui/src/Export/ExportDialog.js
@@ -33,6 +33,8 @@ import { type Person, type Repo } from "./repositories/common";
 import { lift3 } from "../util/optional";
 import { type Tag } from "./repositories/Tags";
 import * as ArrayUtils from "../util/ArrayUtils";
+import * as Parsers from "../util/parsers";
+import Result from "../util/result";
 import { parseEncodedTags } from "../components/Tags/ParseEncodedTagStrings";
 
 const DEFAULT_REPO_CONFIG = {
@@ -283,25 +285,36 @@ function ExportDialog({
       nfsConfig: { ...state.nfsConfig },
     };
     return axios
-      .post<typeof data, {| data: Array<string> |}>(url, data)
+      .post<typeof data, mixed>(url, data)
       .then((response) =>
-        // parse the tag strings and only keep those with metadata
-        ArrayUtils.mapOptional(
-          (tag) =>
-            lift3(
-              (vocabulary, uri, version) => ({
-                value: tag.value,
-                vocabulary,
-                uri,
-                version,
-              }),
-              tag.vocabulary,
-              tag.uri,
-              tag.version
-            ),
-          parseEncodedTags(response.data.data.flatMap((str) => str.split(",")))
-        )
+        Parsers.isObject(response)
+          .flatMap(Parsers.isNotNull)
+          .flatMap(Parsers.getValueWithKey("data"))
+          .flatMap(Parsers.isArray)
+          .flatMap((data) => Result.all(...data.map(Parsers.isString)))
+          .map((data) =>
+            // parse the tag strings and only keep those with metadata
+            ArrayUtils.mapOptional(
+              (tag) =>
+                lift3(
+                  (vocabulary, uri, version) => ({
+                    value: tag.value,
+                    vocabulary,
+                    uri,
+                    version,
+                  }),
+                  tag.vocabulary,
+                  tag.uri,
+                  tag.version
+                ),
+              parseEncodedTags(data.flatMap((str) => str.split(",")))
+            )
+          )
+          .orElse<Array<Tag>>([])
       )
+      .catch(() => {
+        return ([]: Array<Tag>);
+      })
       .finally(() => {
         runInAction(() => {
           state.loading = false;

commit a4f017d680baca601618970841b3c06a3e72c27d

PRT-815 Fix the autosuggesting of tags when exporting documents (#155)
Commit c65e40c6c9 broke the logic that deserialises the API response for
getting the list of tags set on a list of documents, which is used to
suggest the tags that should be included in an export to the
repositories. As such, the tags were silently not being included.

src/main/webapp/ui/src/Export/ExportDialog.js
@@ -299,6 +299,9 @@ function ExportDialog({
         Parsers.isObject(response)
           .flatMap(Parsers.isNotNull)
           .flatMap(Parsers.getValueWithKey("data"))
+          .flatMap(Parsers.isObject)
+          .flatMap(Parsers.isNotNull)
+          .flatMap(Parsers.getValueWithKey("data"))
           .flatMap(Parsers.isArray)
           .flatMap((data) => Result.all(...data.map(Parsers.isString)))
           .map((data) =>
@@ -319,9 +322,14 @@ function ExportDialog({
               parseEncodedTags(data.flatMap((str) => str.split(",")))
             )
           )
+          .mapError(([e]) => {
+            console.error(e);
+            return e;
+          })
           .orElse<Array<Tag>>([])
       )
-      .catch(() => {
+      .catch((e) => {
+        console.error(e);
         return ([]: Array<Tag>);
       })
       .finally(() => {

src/main/webapp/ui/src/Export/__tests__/ExportDialog.test.js
@@ -3,6 +3,7 @@
  */
 //@flow
 /* eslint-env jest */
+import "../../../__mocks__/matchMedia";
 import React, { useState } from "react";
 import {
   render,
@@ -22,6 +23,15 @@ import CREATE_QUICK_EXPORT_PLAN from "./createQuickExportPlan";
 import each from "jest-each";
 import { type UseState } from "../../util/types";
 import Alerts from "../../components/Alerts/Alerts";
+import { sleep } from "../../util/Util";
+
+window.fetch = jest.fn(() =>
+  Promise.resolve({
+    status: 200,
+    ok: true,
+    json: () => Promise.resolve({}),
+  })
+);
 
 const mockAxios = new MockAdapter(axios);
 
@@ -287,6 +297,71 @@ describe("ExportDialog", () => {
           ).toBeVisible();
         });
       });
+    });
+  });
+
+  describe("Controlled vocabulary terms", () => {
+    // passes on its own, fails when run together
+    test("Tags should be pre-populated from the tags on the documents", async () => {
+      mockAxios.onGet("/repository/ajax/repo/uiConfig").reply(200, [
+        {
+          repoName: "app.zenodo",
+          subjects: [],
+          license: {
+            licenseRequired: true,
+            otherLicensePermitted: false,
+            licenses: [
+              {
+                licenseDefinition: {
+                  url: "https://creativecommons.org/publicdomain/zero/1.0/",
+                  name: "CC-0",
+                },
+                defaultLicense: null,
+              },
+            ],
+          },
+          otherProperties: [],
+          linkedDMPs: null,
+          options: {
+            ZENODO_USER_TOKEN:
+              "************************************************************",
+          },
+          displayName: "Zenodo",
+        },
+      ]);
+      mockAxios.onPost("/export/ajax/exportRecordTagsPdfsAndDocs").reply(200, {
+        data: [
+          "BT-20 cell__RSP_EXTONT_URL_DELIM__http:__rspactags_forsl____rspactags_forsl__purl.obolibrary.org__rspactags_forsl__obo__rspactags_forsl__BTO_0001466__RSP_EXTONT_NAME_DELIM__test__RSP_EXTONT_VERSION_DELIM__1",
+        ],
+        error: null,
+        success: true,
+        errorMsg: null,
+      });
+      const { setProps } = renderExportDialog();
+      act(() => {
+        setProps({
+          open: true,
+          selection: {
+            type: "selection",
+            exportTypes: ["NORMAL"],
+            exportNames: ["foo"],
+            exportIds: ["1"],
+          },
+        });
+      });
+
+      fireEvent.click(await screen.findByRole("radio", { name: /PDF/ }));
+      fireEvent.click(
+        screen.getByRole("checkbox", { name: /Export to a repository/ })
+      );
+      fireEvent.click(screen.getByRole("button", { name: /Next/ }));
+      expect(await screen.findByRole("textbox", { name: /File name/ }));
+      fireEvent.click(screen.getByRole("button", { name: /Next/ }));
+      expect(await screen.findByRole("radio", { name: /Zenodo/ }));
+
+      await sleep(1000);
+
+      expect(screen.getByRole("button", { name: /BT-20/ })).toBeVisible();
     });
   });
   describe("Completing the export, makes the right call to /export/ajax/exportArchive", () => {