233f96920048b3bb9ddf5b61fa0ff2545763e397
Robert Lamacraft <robert.lamacraft@researchspace.com>
2024-10-31 14:18:53 +0000

    Layout

--- src/main/webapp/ui/src/eln/gallery/components/Carousel.js
+++ src/main/webapp/ui/src/eln/gallery/components/Carousel.js
@@ -4,6 +4,7 @@ import React, { type Node } from "react";
 import { type GalleryFile, idToString } from "../useGalleryListing";
 import { Optional } from "../../../util/optional";
 import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
 
 type CarouselArgs = {
   listing:
@@ -20,40 +21,50 @@ export default function Carousel({ listing }: CarouselArgs): Node {
 
   if (listing.tag === "empty") return "No files";
   return (
-    <>
-      <Button
-        onClick={() => {
-          setVisibleIndex((v) => Math.max(0, v - 1));
-        }}
-      >
-        Previous
-      </Button>
-      <Button
-        onClick={() => {
-          setVisibleIndex((v) => Math.min(listing.list.length - 1, v + 1));
-        }}
-      >
-        Next
-      </Button>
-      <div
-        style={{
+    <Grid container direction="column" sx={{ height: "100%" }} spacing={2}>
+      <Grid item container direction="row" spacing={1}>
+        <Grid item>
+          <Button
+            onClick={() => {
+              setVisibleIndex((v) => Math.max(0, v - 1));
+            }}
+          >
+            Previous
+          </Button>
+        </Grid>
+        <Grid item flexGrow={1}></Grid>
+        <Grid item>
+          <Button
+            onClick={() => {
+              setVisibleIndex((v) => Math.min(listing.list.length - 1, v + 1));
+            }}
+          >
+            Next
+          </Button>
+        </Grid>
+      </Grid>
+      <Grid
+        item
+        flexGrow={1}
+        sx={{
           position: "relative",
         }}
       >
         {listing.list
           .filter((f) => f.isImage)
           .map((f, i) => (
-            <img
-              src={f.downloadHref}
-              style={{
-                position: "absolute",
-                display: i === visibleIndex ? "block" : "none",
-                width: "100%",
-              }}
-              key={idToString(f.id)}
-            />
-          ))}
-      </div>
-    </>
+          <img
+            src={f.downloadHref}
+            style={{
+              position: "absolute",
+              display: i === visibleIndex ? "block" : "none",
+              maxHeight: "100%",
+              maxWidth: "100%",
+            }}
+            key={idToString(f.id)}
+          />
+        ))}
+      </Grid>
+    </Grid>
   );
 }
