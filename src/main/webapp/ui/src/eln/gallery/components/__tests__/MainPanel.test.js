93740476721bd766eef046c1a642a39b9a83257f
Robert Lamacraft <robert.lamacraft@researchspace.com>
2024-11-26 12:51:14 +0000

    MainPanel a11y test

---
+++ src/main/webapp/ui/src/eln/gallery/components/__tests__/MainPanel.test.js
@@ -0,0 +1,80 @@
+/*
+ * @jest-environment jsdom
+ */
+//@flow
+/* eslint-env jest */
+import "../../../../../__mocks__/matchMedia";
+import React from "react";
+import { render, cleanup, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
+import { axe, toHaveNoViolations } from "jest-axe";
+import MainPanel from "../MainPanel";
+import { ThemeProvider } from "@mui/material/styles";
+import materialTheme from "../../../../theme";
+import { BrowserRouter, Navigate } from "react-router-dom";
+import MockAdapter from "axios-mock-adapter";
+import * as axios from "axios";
+import page1 from "../../__tests__/getUploadedFiles_1";
+
+expect.extend(toHaveNoViolations);
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+afterEach(cleanup);
+
+jest.mock("react-pdf", () => ({
+  Document: () => null,
+  Page: () => {},
+  pdfjs: {
+    GlobalWorkerOptions: {
+      workerSrc: null,
+    },
+  },
+}));
+
+jest.mock("../MoveToIrods", () => ({
+  __esModule: true,
+  default: () => null,
+  COLOR: {
+    main: { hue: 0, lightness: 0, saturation: 0 },
+    background: { hue: 0, lightness: 0, saturation: 0 },
+    contrastText: { hue: 0, lightness: 0, saturation: 0 },
+  },
+}));
+
+const mockAxios = new MockAdapter(axios);
+mockAxios.onGet("/collaboraOnline/supportedExts").reply(200, { data: {} });
+mockAxios.onGet("/officeOnline/supportedExts").reply(200, { data: {} });
+mockAxios
+  .onGet("/export/ajax/defaultPDFConfig")
+  .reply(200, { data: { pageSize: "A4" } });
+mockAxios.onGet("/gallery/getUploadedFiles").reply(200, page1);
+
+describe("MainPanel", () => {
+  test("Has no accessibility violations", async () => {
+    const { container } = render(
+      <BrowserRouter>
+        <ThemeProvider theme={materialTheme}>
+          <MainPanel
+            selectedSection="Images"
+            path={[]}
+            clearPath={() => {}}
+            galleryListing={{ tag: "loading" }}
+            folderId={{ tag: "loading" }}
+            refreshListing={() => {}}
+            sortOrder="DESC"
+            orderBy="modificationDate"
+            setSortOrder={() => {}}
+            setOrderBy={() => {}}
+          />
+        </ThemeProvider>
+      </BrowserRouter>
+    );
+
+    // $FlowExpectedError[incompatible-call] See expect.extend above
+    expect(await axe(container)).toHaveNoViolations();
+  });
+});
