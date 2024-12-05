c624f16350f751d2484a08a5b3e0224ac319367a
Robert Lamacraft <robert.lamacraft@researchspace.com>
2024-12-05 11:43:12 +0000

    Delete filestore

--- src/main/webapp/ui/src/eln/gallery/useGalleryListing.js
+++ src/main/webapp/ui/src/eln/gallery/useGalleryListing.js
@@ -460,7 +460,7 @@ export class LocalGalleryFile implements GalleryFile {
   }
 }
 
-class Filestore implements GalleryFile {
+export class Filestore implements GalleryFile {
   id: Id;
   filesystemId: number;
   filesystemName: string;
@@ -542,7 +542,7 @@ class Filestore implements GalleryFile {
   }
 
   get canDelete(): Result<null> {
-    return Result.Error([new Error("Cannot delete filestores.")]);
+    return Result.Ok(null);
   }
 
   get canRename(): Result<null> {
