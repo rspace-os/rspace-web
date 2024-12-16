rspace-os/rspace-web

commit 0e46f2e6e8cf4e8b273def32e57be80bd34b917f

New Gallery's New Folder dialog should require a non-empty name

src/main/webapp/ui/src/eln/gallery/components/Sidebar.js
@@ -49,13 +49,16 @@ import DialogTitle from "@mui/material/DialogTitle";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogActions from "@mui/material/DialogActions";
-import SubmitSpinnerButton from "../../../components/SubmitSpinnerButton";
 import { useIntegrationIsAllowedAndEnabled } from "../../../common/integrationHelpers";
 import useOneDimensionalRovingTabIndex from "../../../components/useOneDimensionalRovingTabIndex";
 import useViewportDimensions from "../../../util/useViewportDimensions";
 import { observer } from "mobx-react-lite";
 import { autorun } from "mobx";
 import EventBoundary from "../../../components/EventBoundary";
+import ValidatingSubmitButton, {
+  IsValid,
+  IsInvalid,
+} from "../../../components/ValidatingSubmitButton";
 library.add(faImage);
 library.add(faFilm);
 library.add(faFile);
@@ -245,12 +248,17 @@ const NewFolderMenuItem = ({
               >
                 Cancel
               </Button>
-              <SubmitSpinnerButton
-                type="submit"
+              <ValidatingSubmitButton
                 loading={submitting}
-                disabled={submitting}
-                label="Create"
-              />
+                validationResult={
+                  name.length > 0 ? IsValid() : IsInvalid("A name is required.")
+                }
+                onClick={() => {
+                  /* see form onSubmit */
+                }}
+              >
+                Create
+              </ValidatingSubmitButton>
             </DialogActions>
           </form>
         </Dialog>

commit 010a587508299a9a0f88814c4a37570c23625080

Fix new Gallery's New Folder dialog submit button
Commit 0e46f2e6e8 accidentally broke the mouse/pointer submission of the dialog
when validation was added to ensure that the dialog could not be submitted with
an empty name.

src/main/webapp/ui/src/components/ValidatingSubmitButton.js
@@ -93,6 +93,15 @@ export default function ValidatingSubmitButton({
         label={children}
         disabled={loading}
         loading={loading}
+        /*
+         * By using type="submit", any <form> element that wraps this button
+         * will delegate to this button's onClick whenever the user submits the
+         * form, including when they press enter inside of a textfield. As
+         * such, there is no need to implement the submit handler twice, the
+         * <form>'s one can be left undefined. All of this results in the form
+         * submission being more keyboard friendly and thus more accessible to
+         * users who struggle with precise pointer interactions.
+         */
         type="submit"
         progress={progress}
         onClick={(e: Event & { currentTarget: EventTarget, ... }) => {

src/main/webapp/ui/src/eln/gallery/components/Sidebar.js
@@ -214,19 +214,7 @@ const NewFolderMenuItem = ({
             setOpen(false);
           }}
         >
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              setSubmitting(true);
-              void createFolder(path, folderId, name)
-                .then(() => {
-                  onDialogClose(true);
-                })
-                .finally(() => {
-                  setSubmitting(false);
-                });
-            }}
-          >
+          <form /* onSubmit is handled by ValidatingSubmitButton */>
             <DialogTitle>New Folder</DialogTitle>
             <DialogContent>
               <DialogContentText variant="body2" sx={{ mb: 2 }}>
@@ -254,7 +242,14 @@ const NewFolderMenuItem = ({
                   name.length > 0 ? IsValid() : IsInvalid("A name is required.")
                 }
                 onClick={() => {
-                  /* see form onSubmit */
+                  setSubmitting(true);
+                  void createFolder(path, folderId, name)
+                    .then(() => {
+                      onDialogClose(true);
+                    })
+                    .finally(() => {
+                      setSubmitting(false);
+                    });
                 }}
               >
                 Create

src/main/webapp/ui/src/eln/gallery/components/__tests__/Sidebar.test.js
@@ -1,0 +1,160 @@
+/*
+ * @jest-environment jsdom
+ */
+//@flow
+/* eslint-env jest */
+import "../../../../../__mocks__/matchMedia";
+import React from "react";
+import { render, cleanup, screen, act } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
+import Sidebar from "../Sidebar";
+import { ThemeProvider } from "@mui/material/styles";
+import materialTheme from "../../../../theme";
+import MockAdapter from "axios-mock-adapter";
+import * as axios from "axios";
+import { DUMMY_ID } from "../../useGalleryListing";
+
+function newMockAxios() {
+  const mockAxios = new MockAdapter(axios);
+
+  mockAxios.onGet("/integration/integrationInfo?name=DMPTOOL").reply(200, {
+    data: {
+      name: "DMPTOOL",
+      displayName: "DMPtool",
+      available: true,
+      enabled: false,
+      oauthConnected: false,
+      options: {},
+    },
+    error: null,
+    success: true,
+    errorMsg: null,
+  });
+
+  mockAxios.onGet("/integration/integrationInfo?name=DMPONLINE").reply(200, {
+    data: {
+      name: "DMPONLINE",
+      displayName: "DMPonline",
+      available: true,
+      enabled: true,
+      oauthConnected: false,
+      options: {},
+    },
+    error: null,
+    success: true,
+    errorMsg: null,
+  });
+
+  mockAxios.onGet("/integration/integrationInfo?name=ARGOS").reply(200, {
+    data: {
+      name: "ARGOS",
+      displayName: "Argos",
+      available: false,
+      enabled: false,
+      oauthConnected: false,
+      options: {},
+    },
+    error: null,
+    success: true,
+    errorMsg: null,
+  });
+
+  mockAxios.onPost("gallery/ajax/createFolder").reply(200, {
+    data: true,
+    error: null,
+    success: true,
+    errorMsg: null,
+  });
+
+  return mockAxios;
+}
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+afterEach(cleanup);
+
+describe("Sidebar", () => {
+  describe("New Folder", () => {
+    test("Clicking the Submit button should work", async () => {
+      const user = userEvent.setup();
+      const mockAxios = newMockAxios();
+
+      const { baseElement } = render(
+        <ThemeProvider theme={materialTheme}>
+          <Sidebar
+            selectedSection="Images"
+            setSelectedSection={() => {}}
+            drawerOpen
+            setDrawerOpen={() => {}}
+            path={[]}
+            folderId={{ tag: "success", value: DUMMY_ID }}
+            refreshListing={() => {}}
+            id="1"
+          />
+        </ThemeProvider>
+      );
+
+      await act(async () => {
+        await user.click(screen.getByRole("button", { name: "Create" }));
+      });
+
+      const menuitem = await screen.findByRole("menuitem", {
+        name: /New Folder/i,
+      });
+      await act(async () => {
+        await user.click(menuitem);
+      });
+
+      await act(async () => {
+        await user.type(await screen.findByRole("textbox"), "test");
+      });
+
+      await act(async () => {
+        await user.click(
+          await screen.findByRole("button", { name: /create/i })
+        );
+      });
+
+      expect(mockAxios.history.post.length).toBeGreaterThan(0);
+    });
+    test("Pressing enter to Submit should work", async () => {
+      const user = userEvent.setup();
+      const mockAxios = newMockAxios();
+
+      const { baseElement } = render(
+        <ThemeProvider theme={materialTheme}>
+          <Sidebar
+            selectedSection="Images"
+            setSelectedSection={() => {}}
+            drawerOpen
+            setDrawerOpen={() => {}}
+            path={[]}
+            folderId={{ tag: "success", value: DUMMY_ID }}
+            refreshListing={() => {}}
+            id="1"
+          />
+        </ThemeProvider>
+      );
+
+      await act(async () => {
+        await user.click(screen.getByRole("button", { name: "Create" }));
+      });
+
+      const menuitem = await screen.findByRole("menuitem", {
+        name: /New Folder/i,
+      });
+      await act(async () => {
+        await user.click(menuitem);
+      });
+
+      await act(async () => {
+        await user.type(await screen.findByRole("textbox"), "test{enter}");
+      });
+
+      expect(mockAxios.history.post.length).toBeGreaterThan(0);
+    });
+  });
+});