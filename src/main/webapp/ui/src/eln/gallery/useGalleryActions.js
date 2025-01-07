32e3214ad479acd9818f9c8a0a86710dfa97250b
Robert Lamacraft <robert.lamacraft@researchspace.com>
2025-01-07 15:39:20 +0000

    Invert check that file has description before changing
    Commit 54c67dc713 had the boolean check inverted

--- src/main/webapp/ui/src/eln/gallery/useGalleryActions.js
+++ src/main/webapp/ui/src/eln/gallery/useGalleryActions.js
@@ -702,7 +702,7 @@ export function useGalleryActions(): {|
     newDescription: Description
   ) {
     if (
-      file.description.match({
+      !file.description.match({
         missing: () => false,
         empty: () => true,
         present: () => true,
