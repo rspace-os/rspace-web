7b5e020482b8a5a520d61f0487f723a184ad65ed
Robert Lamacraft <robert.lamacraft@researchspace.com>
2025-01-13 10:57:07 +0000

    Clean up EventBoundary
    * Props should be an exact object
    * The component should have a display name, achieved by using a function
    * The documentation had a typo

--- src/main/webapp/ui/src/components/EventBoundary.js
+++ src/main/webapp/ui/src/components/EventBoundary.js
@@ -5,30 +5,36 @@ import React, { type Node } from "react";
 /**
  * All of the DOM events that happen inside of a dialog shouldn't propagate
  * outside as the menu will take them to be events that it should respond to by
- * providing keyboard navigation. See ../../../../QuirksOfMaterialUi.md, secion
- * "Dialogs inside Menus", for more information.
+ * providing keyboard navigation. See ../../../../QuirksOfMaterialUi.md,
+ * section "Dialogs inside Menus", for more information.
  */
-export default ({ children }: { children: Node }): Node => (
-  /*
-   * The eslint suppression is required because `div`s should not ordinarilly
-   * have event handlers as they cannot be focussed. However, in this case, we
-   * are just using the event listeners on the `div` to prevent the events from
-   * further down the DOM from propagating up; the user need not interact with
-   * the `div` itself for this to work.
-   */
+export default function EventBoundary({
+  children,
+}: {|
+  children: Node,
+|}): Node {
+  return (
+    /*
+     * The eslint suppression is required because `div`s should not ordinarilly
+     * have event handlers as they cannot be focussed. However, in this case, we
+     * are just using the event listeners on the `div` to prevent the events from
+     * further down the DOM from propagating up; the user need not interact with
+     * the `div` itself for this to work.
+     */
 
-  // eslint-disable-next-line
-  <div
-    onKeyDown={(e) => {
-      e.stopPropagation();
-    }}
-    onMouseDown={(e) => {
-      e.stopPropagation();
-    }}
-    onClick={(e) => {
-      e.stopPropagation();
-    }}
-  >
-    {children}
-  </div>
-);
+    // eslint-disable-next-line
+    <div
+      onKeyDown={(e) => {
+        e.stopPropagation();
+      }}
+      onMouseDown={(e) => {
+        e.stopPropagation();
+      }}
+      onClick={(e) => {
+        e.stopPropagation();
+      }}
+    >
+      {children}
+    </div>
+  );
+}
