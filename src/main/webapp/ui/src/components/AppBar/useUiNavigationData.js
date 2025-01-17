f031fa9a09c179e95e9f5dba4a8029c7c680e4a1
Robert Lamacraft <robert.lamacraft@researchspace.com>
2025-01-17 14:55:05 +0000

    Fetch nav ui data

--- src/main/webapp/ui/src/components/AppBar/useUiNavigationData.js
+++ src/main/webapp/ui/src/components/AppBar/useUiNavigationData.js
@@ -1,6 +1,10 @@
 //@flow
 
+import React from "react";
 import * as FetchingData from "../../util/fetchingData";
+import axios from "axios";
+import useOauthToken from "../../common/useOauthToken";
+import * as Parsers from "../../util/parsers";
 
 type UiNavigationData = {||};
 
@@ -9,5 +13,52 @@ type UiNavigationData = {||};
  * the AppBar.
  */
 export default function useUiNavigationData(): FetchingData.Fetched<UiNavigationData> {
-  return { tag: "loading" };
+  const { getToken } = useOauthToken();
+  const [loading, setLoading] = React.useState(true);
+  const [uiData, setUiData] = React.useState<null | UiNavigationData>(null);
+  const [errorMessage, setErrrorMessage] = React.useState<null | string>(null);
+
+  async function getUiNavigationData(): Promise<void> {
+    setUiData(null);
+    setLoading(true);
+    setErrrorMessage(null);
+    try {
+      const token = await getToken();
+      const { data } = await axios.get<mixed>(
+        "/api/v1/userDetails/uiNavigationData",
+        {
+          headers: {
+            Authorization: "Bearer " + token,
+          },
+        }
+      );
+      Parsers.isObject(data)
+        .flatMap(Parsers.isNotNull)
+        .map(() => ({}))
+        .do((newUiData) => {
+          setUiData(newUiData);
+        });
+    } catch (e) {
+      console.error(e);
+      setErrrorMessage(e.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  React.useEffect(() => {
+    void getUiNavigationData();
+    /* eslint-disable-next-line react-hooks/exhaustive-deps --
+     * - getUiNavigationData will not meaningfully change
+     */
+  }, []);
+
+  if (loading) return { tag: "loading" };
+  if (errorMessage) return { tag: "error", error: errorMessage };
+  if (!uiData)
+    return {
+      tag: "error",
+      error: "useUiNavigationData is in an invalid state",
+    };
+  return { tag: "success", value: uiData };
 }
