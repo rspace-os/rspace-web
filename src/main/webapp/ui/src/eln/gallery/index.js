rspace-os/rspace-web

commit b91d8582827d7c504dc38a359e13deef63308afb

PRT-880 Don't hard-code height of app bar
Otherwise when there is a banner below it, the rest of the page's scroll
containers don't work correctly.

src/main/webapp/ui/src/eln/gallery/index.js
@@ -55,6 +55,7 @@ import axios from "axios";
 import useOauthToken from "../../common/useOauthToken";
 import RsSet from "../../util/set";
 import docLinks from "../../assets/DocLinks";
+import Stack from "@mui/material/Stack";
 
 const WholePage = styled(
   ({
@@ -173,101 +174,100 @@ const WholePage = styled(
                 });
               }}
             >
-              <AppBar
-                variant="page"
-                currentPage="Gallery"
-                sidebarToggle={
-                  <SidebarToggle
-                    setSidebarOpen={setDrawerOpen}
-                    sidebarOpen={drawerOpen}
-                    sidebarId={sidebarId}
-                  />
-                }
-                accessibilityTips={{
-                  supportsHighContrastMode: true,
-                  supportsReducedMotion: true,
-                  supports2xZoom: true,
-                }}
-              />
-              <Box sx={{ borderBottom: theme.borders.card }}>
-                <Alert
-                  icon={<BroadcastIcon fontSize="inherit" />}
-                  severity="info"
-                >
-                  Welcome to the{" "}
-                  <Link href={docLinks.gallery}>new Gallery!</Link> If you
-                  encounter any issues, please let us know by emailing{" "}
-                  <Link href="mailto:support@researchspace.com">support</Link>{" "}
-                  and using the{" "}
-                  <Link target="_self" href="/oldGallery">
-                    old Gallery
-                  </Link>{" "}
-                  in the meantime.
-                </Alert>
-              </Box>
-              <Box
-                sx={{ display: "flex", height: "calc(100% - 48px)" }}
-                component="main"
-              >
-                <Sidebar
-                  selectedSection={FetchingData.getSuccessValue(
-                    selectedSection
-                  ).orElse(null)}
-                  setSelectedSection={(mediaType) => {
-                    setSelectedSection({ mediaType });
-                    setPath([]);
-                    setAppliedSearchTerm("");
-                    trackEvent("user:change:section:gallery", {
-                      section: mediaType,
-                    });
+              <Stack sx={{ height: "100%" }}>
+                <AppBar
+                  variant="page"
+                  currentPage="Gallery"
+                  sidebarToggle={
+                    <SidebarToggle
+                      setSidebarOpen={setDrawerOpen}
+                      sidebarOpen={drawerOpen}
+                      sidebarId={sidebarId}
+                    />
+                  }
+                  accessibilityTips={{
+                    supportsHighContrastMode: true,
+                    supportsReducedMotion: true,
+                    supports2xZoom: true,
                   }}
-                  drawerOpen={drawerOpen}
-                  setDrawerOpen={setDrawerOpen}
-                  folderId={folderId}
-                  refreshListing={refreshListing}
-                  id={sidebarId}
                 />
-                <Box
-                  sx={{
-                    height: "100%",
-                    display: "flex",
-                    flexDirection: "column",
-                    flexGrow: 1,
-                    minWidth: 0,
-                  }}
-                >
-                  <MainPanel
+                <Box sx={{ borderBottom: theme.borders.card }}>
+                  <Alert
+                    icon={<BroadcastIcon fontSize="inherit" />}
+                    severity="info"
+                  >
+                    Welcome to the{" "}
+                    <Link href={docLinks.gallery}>new Gallery!</Link> If you
+                    encounter any issues, please let us know by emailing{" "}
+                    <Link href="mailto:support@researchspace.com">support</Link>{" "}
+                    and using the{" "}
+                    <Link target="_self" href="/oldGallery">
+                      old Gallery
+                    </Link>{" "}
+                    in the meantime.
+                  </Alert>
+                </Box>
+                <Box sx={{ display: "flex", minHeight: "0" }} component="main">
+                  <Sidebar
                     selectedSection={FetchingData.getSuccessValue(
                       selectedSection
                     ).orElse(null)}
-                    path={FetchingData.getSuccessValue(path).orElse(null)}
                     setSelectedSection={(mediaType) => {
                       setSelectedSection({ mediaType });
                       setPath([]);
                       setAppliedSearchTerm("");
+                      trackEvent("user:change:section:gallery", {
+                        section: mediaType,
+                      });
                     }}
-                    galleryListing={galleryListing}
+                    drawerOpen={drawerOpen}
+                    setDrawerOpen={setDrawerOpen}
                     folderId={folderId}
                     refreshListing={refreshListing}
-                    key={null}
-                    sortOrder={sortOrder}
-                    orderBy={orderBy}
-                    setSortOrder={setSortOrder}
-                    setOrderBy={setOrderBy}
-                    appliedSearchTerm={appliedSearchTerm}
-                    setAppliedSearchTerm={(newTerm) => {
-                      FetchingData.getSuccessValue(path).do((p) => {
-                        if (p.length > 0) {
-                          trackEvent("user:search:folder:gallery");
-                        } else {
-                          trackEvent("user:search:section:gallery");
-                        }
-                      });
-                      setAppliedSearchTerm(newTerm);
-                    }}
+                    id={sidebarId}
                   />
+                  <Box
+                    sx={{
+                      height: "100%",
+                      display: "flex",
+                      flexDirection: "column",
+                      flexGrow: 1,
+                      minWidth: 0,
+                    }}
+                  >
+                    <MainPanel
+                      selectedSection={FetchingData.getSuccessValue(
+                        selectedSection
+                      ).orElse(null)}
+                      path={FetchingData.getSuccessValue(path).orElse(null)}
+                      setSelectedSection={(mediaType) => {
+                        setSelectedSection({ mediaType });
+                        setPath([]);
+                        setAppliedSearchTerm("");
+                      }}
+                      galleryListing={galleryListing}
+                      folderId={folderId}
+                      refreshListing={refreshListing}
+                      key={null}
+                      sortOrder={sortOrder}
+                      orderBy={orderBy}
+                      setSortOrder={setSortOrder}
+                      setOrderBy={setOrderBy}
+                      appliedSearchTerm={appliedSearchTerm}
+                      setAppliedSearchTerm={(newTerm) => {
+                        FetchingData.getSuccessValue(path).do((p) => {
+                          if (p.length > 0) {
+                            trackEvent("user:search:folder:gallery");
+                          } else {
+                            trackEvent("user:search:section:gallery");
+                          }
+                        });
+                        setAppliedSearchTerm(newTerm);
+                      }}
+                    />
+                  </Box>
                 </Box>
-              </Box>
+              </Stack>
             </OpenFolderProvider>
           </CallableAsposePreview>
         </CallablePdfPreview>

commit f5b5e8450f43bedbf2da1f8849cfa31a792c8f8c

PRT-880 Main gallery panel should expand to fill the available space
Bug introduced by b91d8582, because `minHeight: 0` does not specify that the
panel should expand to fill the available space, only that it should not
overflow the available space.

src/main/webapp/ui/src/eln/gallery/index.js
@@ -207,7 +207,10 @@ const WholePage = styled(
                     in the meantime.
                   </Alert>
                 </Box>
-                <Box sx={{ display: "flex", minHeight: "0" }} component="main">
+                <Box
+                  sx={{ display: "flex", minHeight: "0", flexGrow: 1 }}
+                  component="main"
+                >
                   <Sidebar
                     selectedSection={FetchingData.getSuccessValue(
                       selectedSection