06f4420c72e7503839c3b5509fae0178e8fcfa75
Robert Lamacraft <robert.lamacraft@researchspace.com>
2025-03-03 16:34:57 +0000

    Alerts

--- src/main/webapp/ui/src/Inventory/components/Alerts.js
+++ src/main/webapp/ui/src/Inventory/components/Alerts.js
@@ -2,10 +2,36 @@
 
 import React, { type Node, useContext, useEffect } from "react";
 import Alerts from "../../components/Alerts/Alerts";
-import AlertContext, { mkAlert } from "../../stores/contexts/Alert";
+import AlertContext from "../../stores/contexts/Alert";
 import useStores from "../../stores/use-stores";
 
 /*
+ * An inner component is necessary because the Outer component defines an
+ * instance of the Alert react context via its call to the Alert component. To
+ * bind the addAlert and removeAlert functions to UiStore, we need to be
+ * within the part of the component tree that is inside the scope of Alert
+ * context.
+ */
+const Inner = () => {
+  const { addAlert, removeAlert } = useContext(AlertContext);
+  const { uiStore } = useStores();
+
+  useEffect(() => {
+    uiStore.addAlert = addAlert;
+    uiStore.removeAlert = removeAlert;
+    /* eslint-disable-next-line react-hooks/exhaustive-deps --
+     * - addAlert will not change
+     */
+  }, [uiStore]);
+
+  return <> </>;
+};
+
+type OuterArgs = {|
+  children: Node,
+|};
+
+/**
  * This component is an adapter between the Alert context-based approach to
  * showing alerts, as used by the Alerts component, and the original way to
  * display alerts in Inventory using UiStore. This adapter means that we don't
@@ -22,30 +48,6 @@ import useStores from "../../stores/use-stores";
  * including the general Alert component (../../components/Alerts/Alerts) and
  * should be used in the same way.
  */
-
-/*
- * An inner component is necessary because the Outer component defines an
- * instance of the Alert react context via its call to the Alert component. To
- * bind the addAlert and removeAlert functions to UiStore, we need to be
- * within the part of the component tree that is inside the scope of Alert
- * context.
- */
-const Inner = () => {
-  const { addAlert, removeAlert } = useContext(AlertContext);
-  const { uiStore } = useStores();
-
-  useEffect(() => {
-    uiStore.addAlert = addAlert;
-    uiStore.removeAlert = removeAlert;
-  }, [uiStore]);
-
-  return <> </>;
-};
-
-type OuterArgs = {|
-  children: Node,
-|};
-
 export default function Outer({ children }: OuterArgs): Node {
   return (
     <Alerts>
