f251ea05a077899e18a15f3a6b444ab9eb8f1e6e
Robert Lamacraft <robert.lamacraft@researchspace.com>
2025-03-04 17:38:52 +0000

    AlertContext

--- src/main/webapp/ui/src/stores/contexts/Alert.js
+++ src/main/webapp/ui/src/stores/contexts/Alert.js
@@ -16,6 +16,14 @@ type AlertVariant = "success" | "warning" | "error" | "notice";
  */
 let id = 0;
 
+/**
+ * Each alert can have a number of details associated with it, which the user
+ * can view by expanding the alert. The purpose of these details is to provide
+ * more information about which elements being operated on succeeded, which
+ * failed and why. This can form a nice workflow when coupled with the retry
+ * button: some elements fail and are listed in the details, the user can go to
+ * each linked record in turn to correct the issue, then retry the action.
+ */
 export type AlertDetails = {|
   title: string,
   variant: AlertVariant,
@@ -37,6 +45,12 @@ type AlertParams = {|
   icon?: Node,
 |};
 
+/**
+ * An alert that can be displayed to the user. Alerts are displayed in the top
+ * right corner of the viewport. They can be dismissed by the user or be
+ * automatically dismissed after a certain amount of time. They can also have
+ * an action button, a retry button, a number of details, and an icon.
+ */
 export type Alert = {|
   title: ?string,
   message: string,
@@ -54,6 +68,12 @@ export type Alert = {|
   icon: ?Node,
 |};
 
+/**
+ * Smart constructor function for alerts. Various defaults are set here, such
+ * as the variant, duration, and whether the alert is infinite. These can be
+ * overridden by the caller. The id is set automatically and is unique across
+ * all alerts.
+ */
 export function mkAlert(config: AlertParams): Alert {
   return {
     title: config.title,
@@ -132,4 +152,19 @@ const AlertContext: Context<AlertContextType> = createContext(
   DEFAULT_ALERT_CONTEXT
 );
 
+/**
+ * This context allows any part of the page to display alerts in the top right
+ * corner of the viewport, be they indications that an action was successful,
+ * error messages, or anything other timely information that the user should be
+ * alerted of.
+ *
+ * Whilst the context can be used directly to add and remove alerts, it is
+ * recommended that the `Alerts` component[1] be used instead, as it provides
+ * an accessible way to display alerts and ensures that the alerts are
+ * displayed in the correct location. The exception is test code where it may
+ * be advantageous to mock the add and remove functions, rather than rendering
+ * the alerts and asserting the UI.
+ *
+ * [1]: ../../components/Alerts/Alerts.js
+ */
 export default AlertContext;
