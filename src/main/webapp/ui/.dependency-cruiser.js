3a79c1c8cfa60a3fad3c6a07d4571f72c96f36d1
Robert Lamacraft <robert.lamacraft@researchspace.com>
2025-03-05 09:48:06 +0000

    format dependency cruiser

--- src/main/webapp/ui/.dependency-cruiser.js
+++ src/main/webapp/ui/.dependency-cruiser.js
@@ -3,12 +3,13 @@ module.exports = {
   forbidden: [
     {
       name: "compatible-licenses",
-      comment: "Third-party code should adhere to a license that is compatible with our own",
+      comment:
+        "Third-party code should adhere to a license that is compatible with our own",
       severity: "error",
       from: {},
       to: {
-        licenseNot: ["MIT", "BSD", "Apache", "Hippocratic", "ISC"]
-      }
+        licenseNot: ["MIT", "BSD", "Apache", "Hippocratic", "ISC"],
+      },
     },
     {
       name: "axios-only",
@@ -36,34 +37,32 @@ module.exports = {
     //   }
     // },
     {
-      name: 'no-orphans',
+      name: "no-orphans",
       comment:
         "This is an orphan module - it's likely not used (anymore?). Either use it or " +
         "remove it. If it's logical this module is an orphan (i.e. it's a config file), " +
         "add an exception for it in your dependency-cruiser configuration. By default " +
         "this rule does not scrutinize dot-files (e.g. .eslintrc.js), TypeScript declaration " +
         "files (.d.ts), tsconfig.json and some of the babel and webpack configs.",
-      severity: 'warn',
+      severity: "warn",
       from: {
         orphan: true,
         pathNot: [
-          '(^|/)\.[^/]+\.(js|cjs|mjs|ts|json)$', // dot files
-          '\.d\.ts$',                            // TypeScript declaration files
-          '(^|/)tsconfig\.json$',                 // TypeScript config
-          '(^|/)(babel|webpack)\.config\.(js|cjs|mjs|ts|json)$' // other configs
-        ]
+          "(^|/).[^/]+.(js|cjs|mjs|ts|json)$", // dot files
+          ".d.ts$", // TypeScript declaration files
+          "(^|/)tsconfig.json$", // TypeScript config
+          "(^|/)(babel|webpack).config.(js|cjs|mjs|ts|json)$", // other configs
+        ],
       },
       to: {},
     },
     {
-      "name": "no-unreachable-from-root",
-      "severity": "error",
-      "from": {
-        path: [
-          "src",
-        ] 
+      name: "no-unreachable-from-root",
+      severity: "error",
+      from: {
+        path: ["src"],
       },
-      "to": {
+      to: {
         path: "src",
         pathNot: [
           "__tests__|__mocks__|test-stubs|node_modules",
@@ -113,108 +112,103 @@ module.exports = {
           // src/assets contains icon variants that may be useful in the future
           "^src/assets",
         ],
-        reachable: false
-      }
+        reachable: false,
+      },
     },
     {
-      name: 'no-deprecated-core',
+      name: "no-deprecated-core",
       comment:
-        'A module depends on a node core module that has been deprecated. Find an alternative - these are ' +
+        "A module depends on a node core module that has been deprecated. Find an alternative - these are " +
         "bound to exist - node doesn't deprecate lightly.",
-      severity: 'warn',
+      severity: "warn",
       from: {},
       to: {
-        dependencyTypes: [
-          'core'
-        ],
+        dependencyTypes: ["core"],
         path: [
-          '^(v8/tools/codemap)$',
-          '^(v8/tools/consarray)$',
-          '^(v8/tools/csvparser)$',
-          '^(v8/tools/logreader)$',
-          '^(v8/tools/profile_view)$',
-          '^(v8/tools/profile)$',
-          '^(v8/tools/SourceMap)$',
-          '^(v8/tools/splaytree)$',
-          '^(v8/tools/tickprocessor-driver)$',
-          '^(v8/tools/tickprocessor)$',
-          '^(node-inspect/lib/_inspect)$',
-          '^(node-inspect/lib/internal/inspect_client)$',
-          '^(node-inspect/lib/internal/inspect_repl)$',
-          '^(async_hooks)$',
-          '^(punycode)$',
-          '^(domain)$',
-          '^(constants)$',
-          '^(sys)$',
-          '^(_linklist)$',
-          '^(_stream_wrap)$'
+          "^(v8/tools/codemap)$",
+          "^(v8/tools/consarray)$",
+          "^(v8/tools/csvparser)$",
+          "^(v8/tools/logreader)$",
+          "^(v8/tools/profile_view)$",
+          "^(v8/tools/profile)$",
+          "^(v8/tools/SourceMap)$",
+          "^(v8/tools/splaytree)$",
+          "^(v8/tools/tickprocessor-driver)$",
+          "^(v8/tools/tickprocessor)$",
+          "^(node-inspect/lib/_inspect)$",
+          "^(node-inspect/lib/internal/inspect_client)$",
+          "^(node-inspect/lib/internal/inspect_repl)$",
+          "^(async_hooks)$",
+          "^(punycode)$",
+          "^(domain)$",
+          "^(constants)$",
+          "^(sys)$",
+          "^(_linklist)$",
+          "^(_stream_wrap)$",
         ],
-      }
+      },
     },
     {
-      name: 'not-to-deprecated',
+      name: "not-to-deprecated",
       comment:
-        'This module uses a (version of an) npm module that has been deprecated. Either upgrade to a later ' +
-        'version of that module, or find an alternative. Deprecated modules are a security risk.',
-      severity: 'warn',
+        "This module uses a (version of an) npm module that has been deprecated. Either upgrade to a later " +
+        "version of that module, or find an alternative. Deprecated modules are a security risk.",
+      severity: "warn",
       from: {},
       to: {
-        dependencyTypes: [
-          'deprecated'
-        ]
-      }
+        dependencyTypes: ["deprecated"],
+      },
     },
     {
-      name: 'no-duplicate-dep-types',
+      name: "no-duplicate-dep-types",
       comment:
         "Likely this module depends on an external ('npm') package that occurs more than once " +
         "in your package.json i.e. bot as a devDependencies and in dependencies. This will cause " +
         "maintenance problems later on.",
-      severity: 'warn',
+      severity: "warn",
       from: {},
       to: {
         moreThanOneDependencyType: true,
         // as it's pretty common to have a type import be a type only import
         // _and_ (e.g.) a devDependency - don't consider type-only dependency
         // types for this rule
-        dependencyTypesNot: ["type-only"]
-      }
+        dependencyTypesNot: ["type-only"],
+      },
     },
     {
-      name: 'not-to-spec',
+      name: "not-to-spec",
       comment:
-        'This module depends on a spec (test) file. The sole responsibility of a spec file is to test code. ' +
+        "This module depends on a spec (test) file. The sole responsibility of a spec file is to test code. " +
         "If there's something in a spec that's of use to other modules, it doesn't have that single " +
-        'responsibility anymore. Factor it out into (e.g.) a separate utility/ helper or a mock.',
-      severity: 'error',
+        "responsibility anymore. Factor it out into (e.g.) a separate utility/ helper or a mock.",
+      severity: "error",
       from: {},
       to: {
-        path: '\.(spec|test)\.(js|mjs|cjs|ts|ls|coffee|litcoffee|coffee\.md)$'
-      }
+        path: ".(spec|test).(js|mjs|cjs|ts|ls|coffee|litcoffee|coffee.md)$",
+      },
     },
     {
-      name: 'not-to-dev-dep',
-      severity: 'error',
+      name: "not-to-dev-dep",
+      severity: "error",
       comment:
         "This module depends on an npm package from the 'devDependencies' section of your " +
-        'package.json. It looks like something that ships to production, though. To prevent problems ' +
+        "package.json. It looks like something that ships to production, though. To prevent problems " +
         "with npm packages that aren't there on production declare it (only!) in the 'dependencies'" +
-        'section of your package.json. If this module is development only - add it to the ' +
-        'from.pathNot re of the not-to-dev-dep rule in the dependency-cruiser configuration',
+        "section of your package.json. If this module is development only - add it to the " +
+        "from.pathNot re of the not-to-dev-dep rule in the dependency-cruiser configuration",
       from: {
-        path: '^(src)',
-        pathNot: '__tests__|\.(spec|test)\.(js|mjs|cjs|ts|ls|coffee|litcoffee|coffee\.md)$'
+        path: "^(src)",
+        pathNot:
+          "__tests__|.(spec|test).(js|mjs|cjs|ts|ls|coffee|litcoffee|coffee.md)$",
       },
       to: {
-        dependencyTypes: [
-          'npm-dev'
-        ],
-        pathNot: '@babel'
-      }
+        dependencyTypes: ["npm-dev"],
+        pathNot: "@babel",
+      },
     },
     {
-      name: 'optional-deps-used',
-      severity: 'info',
+      name: "optional-deps-used",
+      severity: "info",
       comment:
         "This module depends on an npm package that is declared as an optional dependency " +
         "in your package.json. As this makes sense in limited situations only, it's flagged here. " +
@@ -222,93 +216,96 @@ module.exports = {
         "dependency-cruiser configuration.",
       from: {},
       to: {
-        dependencyTypes: [
-          'npm-optional'
-        ]
-      }
+        dependencyTypes: ["npm-optional"],
+      },
     },
     {
-      name: 'peer-deps-used',
+      name: "peer-deps-used",
       comment:
         "This module depends on an npm package that is declared as a peer dependency " +
         "in your package.json. This makes sense if your package is e.g. a plugin, but in " +
         "other cases - maybe not so much. If the use of a peer dependency is intentional " +
         "add an exception to your dependency-cruiser configuration.",
-      severity: 'warn',
+      severity: "warn",
       from: {},
       to: {
-        dependencyTypes: [
-          'npm-peer'
-        ]
-      }
+        dependencyTypes: ["npm-peer"],
+      },
     },
 
     {
       name: "Inventory-only",
-      comment: "Modules containing generic code or ELN code should not depend on Inventory components or Inventory-specific code.",
+      comment:
+        "Modules containing generic code or ELN code should not depend on Inventory components or Inventory-specific code.",
       from: {
         // only this whitelist of paths...
-        pathNot: "src/Inventory|src/Router.js|src/eln-inventory-integration|src/stores|src/App.js|src/components/PublicPages/IdentifierPublicPage.js",
+        pathNot:
+          "src/Inventory|src/Router.js|src/eln-inventory-integration|src/stores|src/App.js|src/components/PublicPages/IdentifierPublicPage.js",
       },
       to: {
         // ...are the modules that should be accessing these Inventory-specific modules
         path: "src/Inventory|src/stores/stores|src/stores/use-stores",
-      }
+      },
     },
 
     {
       name: "Apps-only",
-      comment: "Modules containing generic code, Inventory code, or other parts of the ELN should not depend on code specific to the apps page.",
+      comment:
+        "Modules containing generic code, Inventory code, or other parts of the ELN should not depend on code specific to the apps page.",
       from: {
-        pathNot: "src/eln/apps"
+        pathNot: "src/eln/apps",
       },
       to: {
-        path: "src/eln/apps"
-      }
+        path: "src/eln/apps",
+      },
     },
 
     {
       name: "Sysadmin-only",
-      comment: "Modules containing generic code, Inventory code, or other parts of the ELN should not depend on code specific to the sysadmin page.",
+      comment:
+        "Modules containing generic code, Inventory code, or other parts of the ELN should not depend on code specific to the sysadmin page.",
       from: {
-        pathNot: "src/eln/sysadmin"
+        pathNot: "src/eln/sysadmin",
       },
       to: {
-        path: "src/eln/sysadmin"
-      }
+        path: "src/eln/sysadmin",
+      },
     },
 
     {
       name: "definitions-are-leaves",
-      comment: "src/stores/definitions should only depend on themselves or utils",
+      comment:
+        "src/stores/definitions should only depend on themselves or utils",
       from: {
         path: "src/stores/definitions",
       },
       to: {
         pathNot: "src/stores/definitions|src/util|fast-check|babel",
-      }
+      },
     },
 
     {
       name: "utils-are-leaves",
-      comment: "src/util should not depend on other parts of the codebase; they should be pure functions and simple data structures.",
+      comment:
+        "src/util should not depend on other parts of the codebase; they should be pure functions and simple data structures.",
       from: {
         path: "src/util",
       },
       to: {
         pathNot: "src/util|babel|jest-dom|fast-check|mobx|react",
-      }
+      },
     },
 
     {
       name: "Public pages must not use stores",
-      comment: "Users may not be authenticated on the public pages and so we must not depend on the global stores as they assume the user is.",
+      comment:
+        "Users may not be authenticated on the public pages and so we must not depend on the global stores as they assume the user is.",
       from: {
         path: "src/components/PublicPages",
       },
       to: {
         path: "src/stores/stores",
-        reachable: true
+        reachable: true,
       },
     },
 
@@ -318,28 +315,24 @@ module.exports = {
       from: {
         pathNot: [
           // once the old gallery is removed, we can remove the dependency and stop using `--force` when calling `npm install`
-          "^src/Gallery"
-        ]
+          "^src/Gallery",
+        ],
       },
       to: {
         path: "@toast-ui/react-image-editor",
-      }
-    }
-
+      },
+    },
   ],
-  allowed: [
-  ],
-  required: [
-  ],
+  allowed: [],
+  required: [],
   options: {
-
     /* conditions specifying which files not to follow further when encountered:
        - path: a regular expression to match
        - dependencyTypes: see https://github.com/sverweij/dependency-cruiser/blob/main/doc/rules-reference.md#dependencytypes-and-dependencytypesnot
        for a complete list
     */
     doNotFollow: {
-      path: 'node_modules'
+      path: "node_modules",
     },
 
     /* conditions specifying which dependencies to exclude
@@ -416,11 +409,11 @@ module.exports = {
        your webpack config is a function and takes them (see webpack documentation
        for details)
      */
-    // webpackConfig: {
-    //  fileName: 'webpack.config.js',
-    //  env: {},
-    //  arguments: {}
-    // },
+    webpackConfig: {
+      fileName: "webpack.config.js",
+      env: {},
+      arguments: {},
+    },
 
     /* Babel config ('.babelrc', '.babelrc.json', '.babelrc.json5', ...) to use
       for compilation (and whatever other naughty things babel plugins do to
@@ -429,7 +422,7 @@ module.exports = {
       systems) without dependency-cruiser getting a major version bump.
      */
     babelConfig: {
-      fileName: 'babel.config.js'
+      fileName: "babel.config.js",
     },
 
     /* List of strings you have in use in addition to cjs/ es6 requires
@@ -491,7 +484,7 @@ module.exports = {
            collapses everything in node_modules to one folder deep so you see
            the external modules, but not the innards your app depends upon.
          */
-        collapsePattern: 'node_modules/(@[^/]+/[^/]+|[^/]+)',
+        collapsePattern: "node_modules/(@[^/]+/[^/]+|[^/]+)",
 
         /* Options to tweak the appearance of your graph.See
            https://github.com/sverweij/dependency-cruiser/blob/main/doc/options-reference.md#reporteroptions
@@ -572,7 +565,8 @@ module.exports = {
           dependency graph reporter (`archi`) you probably want to tweak
           this collapsePattern to your situation.
         */
-        collapsePattern: '^(packages|src|lib|app|bin|test(s?)|spec(s?))/[^/]+|node_modules/(@[^/]+/[^/]+|[^/]+)',
+        collapsePattern:
+          "^(packages|src|lib|app|bin|test(s?)|spec(s?))/[^/]+|node_modules/(@[^/]+/[^/]+|[^/]+)",
 
         /* Options to tweak the appearance of your graph.See
            https://github.com/sverweij/dependency-cruiser/blob/main/doc/options-reference.md#reporteroptions
@@ -583,10 +577,10 @@ module.exports = {
         // theme: {
         // },
       },
-      "text": {
-        "highlightFocused": true
+      text: {
+        highlightFocused: true,
       },
-    }
-  }
+    },
+  },
 };
 // generated: dependency-cruiser@14.1.2 on 2023-10-20T14:17:19.415Z
