75b1e19b730ab5e9a14f7387e39f2d6b7f138ebf
Robert Lamacraft <robert.lamacraft@researchspace.com>
2025-03-11 11:39:06 +0000

    useLocalStorage

--- src/main/webapp/ui/src/util/useLocalStorage.ts
+++ src/main/webapp/ui/src/util/useLocalStorage.ts
@@ -1,9 +1,11 @@
-//@flow
 import { useState } from "react";
 import { type UseState } from "./types";
 
 /**
- * Like useState, but synchronised with local storage 
+ * Like useState, but synchronised with local storage.
+ *
+ * Note that the code assumes that the value stored in local storage is a valid
+ * JSON string of the same type as the default value.
  */
 export default function useLocalStorage<T>(
   key: string,
@@ -11,13 +13,14 @@ export default function useLocalStorage<T>(
 ): UseState<T> {
   const [storedValue, setStoredValue] = useState(() => {
     const item = window.localStorage.getItem(key);
-    return item ? JSON.parse(item) : defaultValue;
+    return item ? (JSON.parse(item) as T) : defaultValue;
   });
 
-  const setValue = (value: ((T) => T) | T): void => {
+  const setValue = (value: ((oldValue: T) => T) | T): void => {
     const valueToStore =
-      // $FlowExpectedError[incompatible-use] T can't be a function, so cast is fine
-      typeof value === "function" ? (value: (T) => T)(storedValue) : (value: T);
+      typeof value === "function"
+        ? (value as (oldValue: T) => T)(storedValue)
+        : value;
 
     setStoredValue(valueToStore);
     window.localStorage.setItem(key, JSON.stringify(valueToStore));
