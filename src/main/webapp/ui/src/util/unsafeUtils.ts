33050d73dc8513c4e94bfe7119e0fbe83f1116fa
Robert Lamacraft <robert.lamacraft@researchspace.com>
2025-03-14 11:23:44 +0000

    util/unsafeUtils

--- src/main/webapp/ui/src/util/unsafeUtils.ts
+++ src/main/webapp/ui/src/util/unsafeUtils.ts
@@ -5,24 +5,24 @@
  * and so any code that utilises them does not have as strong type guarantees.
  */
 
+/* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-assignment */
+
 // be careful when using as it erases Flow types
 export const pick =
-  (...props: Array<string>): ((any) => any) =>
-  (o: { ... }): { ... } =>
-    // $FlowExpectedError[invalid-computed-prop]
+  (...props: Array<string>): ((o: any) => any) =>
+  (o: Record<string, any>): object =>
     props.reduce((a, e) => ({ ...a, [e]: o[e] }), {});
 
 // navigate tree of JSON objects using a period-delimited string
 // e.g. traverseObjectTree({foo: {bar: 4}}, "foo.bar", null) === 4
 export const traverseObjectTree = (
-  obj: {},
+  obj: Record<string, any>,
   prop: string,
   defval: any = null
 ): any => {
   let o = obj;
   const path = prop.split(".");
   for (let i = 0; i < path.length; i++) {
-    // $FlowExpectedError[invalid-computed-prop]
     if (typeof o[path[i]] === "undefined") return defval;
     o = o[path[i]];
   }
