5d02e6102de202d6b0cce85cb5b048f225a5a5e8
Robert Lamacraft <robert.lamacraft@researchspace.com>
2025-03-17 11:25:49 +0000

    create new page at /identifiers/igsn

---
+++ src/main/webapp/ui/src/Inventory/Identifiers/IGSN/index.js
@@ -0,0 +1,15 @@
+// @flow
+
+import React, { type Node } from "react";
+
+export default function IgsnManagementPage(): Node {
+  return (
+    <div>
+      <h1>IGSN Management</h1>
+      <p>
+        This is the IGSN management page. Here, users can mint, register, and
+        reserve IGSNs.
+      </p>
+    </div>
+  );
+}
