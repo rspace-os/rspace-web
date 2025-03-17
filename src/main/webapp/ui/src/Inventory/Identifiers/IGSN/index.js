ec54b36ac30cda3698bee457c044e775b8d31727
Robert Lamacraft <robert.lamacraft@researchspace.com>
2025-03-17 11:43:05 +0000

    basic layout of IGSNs page

--- src/main/webapp/ui/src/Inventory/Identifiers/IGSN/index.js
+++ src/main/webapp/ui/src/Inventory/Identifiers/IGSN/index.js
@@ -1,15 +1,25 @@
 // @flow
 
 import React, { type Node } from "react";
+import Header from "../../components/Layout/Header";
+import Sidebar from "../../components/Layout/Sidebar";
+import Main from "../../Main";
+import Box from "@mui/material/Box";
 
+/**
+ * This is the page where researchers can manage their IGSNs.
+ */
 export default function IgsnManagementPage(): Node {
+  const sidebarId = React.useId();
   return (
-    <div>
-      <h1>IGSN Management</h1>
-      <p>
-        This is the IGSN management page. Here, users can mint, register, and
-        reserve IGSNs.
-      </p>
-    </div>
+    <>
+      <Header sidebarId={sidebarId} />
+      <Box sx={{ display: "flex", height: "calc(100% - 48px)" }}>
+        <Sidebar id={sidebarId} />
+        <Main>
+          <h1>IGSN Management</h1>
+        </Main>
+      </Box>
+    </>
   );
 }
