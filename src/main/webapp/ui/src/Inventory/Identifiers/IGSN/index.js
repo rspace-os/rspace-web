2d6021a6b6473f3e4bd7244075e0494e3b404c80
Robert Lamacraft <robert.lamacraft@researchspace.com>
2025-03-17 11:57:14 +0000

    Header section

--- src/main/webapp/ui/src/Inventory/Identifiers/IGSN/index.js
+++ src/main/webapp/ui/src/Inventory/Identifiers/IGSN/index.js
@@ -5,6 +5,11 @@ import Header from "../../components/Layout/Header";
 import Sidebar from "../../components/Layout/Sidebar";
 import Main from "../../Main";
 import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import TitledBox from "../../components/TitledBox";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import docLinks from "../../../assets/DocLinks";
 
 /**
  * This is the page where researchers can manage their IGSNs.
@@ -17,7 +22,24 @@ export default function IgsnManagementPage(): Node {
       <Box sx={{ display: "flex", height: "calc(100% - 48px)" }}>
         <Sidebar id={sidebarId} />
         <Main>
-          <h1>IGSN Management</h1>
+          <Stack spacing={2} sx={{ mt: 2, mr: 1 }}>
+            <TitledBox title="IGSNs" border>
+              <Typography>
+                The RSpace IGSN ID integration enables researchers to create,
+                publish and update IGSN ID metadata all within Inventory. IGSN
+                IDs describe material samples and features-of-interest, and are
+                provided through the DataCite DOI infrastructure. To learn more,{" "}
+                <Link
+                  target="_blank"
+                  rel="noreferrer"
+                  href={docLinks.IGSNIdentifiers}
+                >
+                  see the IGSN ID documentation
+                </Link>
+                .
+              </Typography>
+            </TitledBox>
+          </Stack>
         </Main>
       </Box>
     </>
