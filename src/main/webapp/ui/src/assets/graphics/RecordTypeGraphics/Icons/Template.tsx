a49f2d1dd0d71d019908294987374312d17f8d64
Robert Lamacraft <robert.lamacraft@researchspace.com>
2025-03-19 16:52:17 +0000

    Template icon

---
+++ src/main/webapp/ui/src/assets/graphics/RecordTypeGraphics/Icons/Template.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import SvgIcon from "@mui/material/SvgIcon";
+import { withStyles } from "Styles";
+
+type TemplateIconArgs = {
+  style?: object;
+  color?: string;
+};
+
+const TemplateIcon = withStyles<TemplateIconArgs, { root: string }>(
+  (theme, { color }) => ({
+    root: {
+      color,
+      fontSize: "inherit",
+      verticalAlign: "-0.2em",
+      width: 14,
+      height: 14,
+    },
+  })
+)(({ classes, style }) => (
+  <SvgIcon className={classes.root} viewBox="0 0 16 16" style={style}>
+    <path d="M13 0H2c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h10c1.1 0 2-.9 2-2V2c0-1.1-.9-2-2-2zm-1 2v2h-4v8h-2v-8h-4v-2h10z" />
+  </SvgIcon>
+));
+
+TemplateIcon.displayName = "TemplateIcon";
+export default TemplateIcon;
