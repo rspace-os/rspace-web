15cf2ac88a24408587fbb0f53d019ae497bc04c6
Robert Lamacraft <robert.lamacraft@researchspace.com>
2025-03-19 10:55:27 +0000

    sysadmin

---
+++ src/main/webapp/ui/src/assets/branding/rspace/sysadmin/index.ts
@@ -0,0 +1,31 @@
+/**
+ * The colours used to accent the UI.
+ */
+export const ACCENT_COLOR = {
+  main: {
+    hue: 197,
+    saturation: 50,
+    lightness: 80,
+  },
+  darker: {
+    hue: 197,
+    saturation: 100,
+    lightness: 30,
+  },
+  contrastText: {
+    hue: 200,
+    saturation: 30,
+    lightness: 36,
+  },
+  background: {
+    hue: 200,
+    saturation: 20,
+    lightness: 82,
+  },
+  backgroundContrastText: {
+    hue: 203,
+    saturation: 17,
+    lightness: 35,
+  },
+};
+
