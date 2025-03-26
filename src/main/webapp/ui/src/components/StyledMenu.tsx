6b174257ca46eab220d096ff71b73c3ed00b6809
Robert Lamacraft <robert.lamacraft@researchspace.com>
2025-03-26 11:41:14 +0000

    components/StyledMenu

--- src/main/webapp/ui/src/components/StyledMenu.tsx
+++ src/main/webapp/ui/src/components/StyledMenu.tsx
@@ -1,12 +1,10 @@
-//@flow
-
-import React, { type ComponentType, type ElementProps } from "react";
+import React from "react";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import { withStyles } from "Styles";
 
-export const StyledMenu: ComponentType<{}> = withStyles<
-  ElementProps<typeof Menu>,
+export const StyledMenu = withStyles<
+  React.ComponentProps<typeof Menu>,
   { paper: string }
 >((theme) => ({
   paper: {
