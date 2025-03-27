2b3770fecc8372b2ee3b08f14fead8c046945e96
Robert Lamacraft <robert.lamacraft@researchspace.com>
2025-03-27 10:37:56 +0000

    useCheckVerificationPasswordNeeded

--- src/main/webapp/ui/src/common/useCheckVerificationPasswordNeeded.ts
+++ src/main/webapp/ui/src/common/useCheckVerificationPasswordNeeded.ts
@@ -1,21 +1,23 @@
-//@flow
-
 import React from "react";
 import axios from "@/common/axios";
 import * as FetchingData from "../util/fetchingData";
 
 export default function useCheckVerificationPasswordNeeded(): FetchingData.Fetched<boolean> {
-  const [isNeeded, setIsNeeded] = React.useState({ tag: "loading" });
+  const [isNeeded, setIsNeeded] = React.useState<FetchingData.Fetched<boolean>>(
+    { tag: "loading" }
+  );
 
   React.useEffect(() => {
     void (async () => {
       try {
-        const { data } = await axios.get<{| data: boolean |}>(
+        const { data } = await axios.get<{ data: boolean }>(
           "/vfpwd/ajax/checkVerificationPasswordNeeded"
         );
         setIsNeeded({ tag: "success", value: data.data });
       } catch (error) {
-        setIsNeeded({ tag: "error", error: error.message });
+        if (error instanceof Error) {
+          setIsNeeded({ tag: "error", error: error.message });
+        }
       }
     })();
   }, []);
