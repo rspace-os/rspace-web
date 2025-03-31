20ddd56341d8767459d00caca3a13daaa12f0345
Robert Lamacraft <robert.lamacraft@researchspace.com>
2025-03-31 15:43:27 +0100

    delete

--- src/main/webapp/ui/src/Inventory/Identifiers/IGSN/index.js
+++ src/main/webapp/ui/src/Inventory/Identifiers/IGSN/index.js
@@ -6,18 +6,20 @@ import Sidebar from "../../components/Layout/Sidebar";
 import IgsnManagementPage from "./IgsnManagementPage";
 import Box from "@mui/material/Box";
 import RsSet from "../../../util/set";
+import { type Identifier } from "../../useIdentifiers";
 
 /**
  * This is the page where researchers can manage their IGSNs.
  */
 export default function IGSN(): Node {
   const sidebarId = React.useId();
+  const [selectedIgsns, setSelectedIgsns] = React.useState(new RsSet<Identifier>());
   return (
     <>
       <Header sidebarId={sidebarId} />
       <Box sx={{ display: "flex", height: "calc(100% - 48px)" }}>
         <Sidebar id={sidebarId} />
-        <IgsnManagementPage selectedIgsns={new RsSet()} setSelectedIgsns={() => {}}/>
+        <IgsnManagementPage selectedIgsns={selectedIgsns} setSelectedIgsns={setSelectedIgsns}/>
       </Box>
     </>
   );
