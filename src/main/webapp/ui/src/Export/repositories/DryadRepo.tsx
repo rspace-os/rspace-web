f62864bf1d8ab4200a9475de26beef5665f26b6a
Robert Lamacraft <robert.lamacraft@researchspace.com>
2025-04-07 17:41:00 +0100

    Export/repositories/DryadRepo

--- src/main/webapp/ui/src/Export/repositories/DryadRepo.tsx
+++ src/main/webapp/ui/src/Export/repositories/DryadRepo.tsx
@@ -1,9 +1,9 @@
-// @flow
-
-import { default as React, type Node, type ComponentType } from "react";
+import React from "react";
 import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
-import Autocomplete from "@mui/material/Autocomplete";
+import Autocomplete, {
+  AutocompleteInputChangeReason,
+} from "@mui/material/Autocomplete";
 import MenuItem from "@mui/material/MenuItem";
 import Users from "../ExportRepoUser";
 import { type Person, type Repo, type StandardValidations } from "./common";
@@ -11,34 +11,40 @@ import { type Person, type Repo, type StandardValidations } from "./common";
 /**
  * Boolean flags for the valid state of the Dryad metadata fields.
  */
-export type DryadValidations = {|
-  ...StandardValidations,
-  crossrefFunder: boolean,
-|};
+export type DryadValidations = StandardValidations & {
+  crossrefFunder: boolean;
+};
 
-type DryadRepoArgs = {|
-  repo: Repo,
+type DryadRepoArgs = {
+  repo: Repo;
   handleChange: (event: {
     target: {
-      name: "title" | "description" | "subject" | "license",
-      value: string,
-    },
-  }) => void,
-  handleCrossrefFunderChange: (mixed, { name: string }) => void,
+      name: "title" | "description" | "subject" | "license";
+      value: string;
+    };
+  }) => void;
+  handleCrossrefFunderChange: (
+    _unused: unknown,
+    event: { name: string }
+  ) => void;
   handleFetchCrossrefFunder: (
-    event: { target: { value: string } },
+    event: React.SyntheticEvent<Element, Event>,
     value: string,
-    reason: string
-  ) => void,
-  crossrefFunders: Array<{ name: string }>,
-  inputValidations: DryadValidations,
-  submitAttempt: boolean,
-  updatePeople: (people: Array<Person>) => void,
-  contacts: Array<Person>,
-  authors: Array<Person>,
-|};
+    reason: AutocompleteInputChangeReason
+  ) => void;
+  crossrefFunders: Array<{ name: string }>;
+  inputValidations: DryadValidations;
+  submitAttempt: boolean;
+  updatePeople: (people: Array<Person>) => void;
+  contacts: Array<Person>;
+  authors: Array<Person>;
+};
 
-function DryadRepo({
+/**
+ * This components provides a form for the user to fill in the details of the
+ * deposit that will be made with Dryad.
+ */
+export default function DryadRepo({
   repo,
   handleChange,
   handleCrossrefFunderChange,
@@ -49,7 +55,7 @@ function DryadRepo({
   updatePeople,
   contacts,
   authors,
-}: DryadRepoArgs): Node {
+}: DryadRepoArgs): React.ReactNode {
   return (
     <Grid container style={{ width: "100%" }}>
       <Grid item xs={12}>
@@ -57,6 +63,7 @@ function DryadRepo({
           error={submitAttempt && !inputValidations.title}
           name="title"
           label="Title *"
+          // @ts-expect-error React event handlers are not parameterised by the name prop
           onChange={handleChange}
           margin="normal"
           helperText="Please choose a title, >3 symbols"
@@ -70,6 +77,7 @@ function DryadRepo({
           label="Add an abstract *"
           multiline
           maxRows="4"
+          // @ts-expect-error React event handlers are not parameterised by the name prop
           onChange={handleChange}
           margin="normal"
           helperText="Please add a relevant abstract for your export"
@@ -83,6 +91,7 @@ function DryadRepo({
           select
           defaultValue={""}
           label="Research Domain *"
+          // @ts-expect-error React event handlers are not parameterised by the name prop
           onChange={handleChange}
           helperText="Please select your research domain"
           margin="normal"
@@ -101,6 +110,7 @@ function DryadRepo({
           select
           label="License *"
           defaultValue={0}
+          // @ts-expect-error React event handlers are not parameterised by the name prop
           onChange={handleChange}
           helperText="Please select your license"
           margin="normal"
@@ -120,7 +130,9 @@ function DryadRepo({
           label="Granting Organization *"
           options={crossrefFunders}
           getOptionLabel={(option) => option.name}
+          // @ts-expect-error An undocumented API, it seems
           getOptionSelected={(option, value) => option.name === value.name}
+          // @ts-expect-error React event handlers are not parameterised by the name prop
           onChange={handleCrossrefFunderChange}
           onInputChange={handleFetchCrossrefFunder}
           sx={{ width: 300 }}
@@ -143,9 +155,3 @@ function DryadRepo({
     </Grid>
   );
 }
-
-/**
- * This components provides a form for the user to fill in the details of the
- * deposit that will be made with Dryad.
- */
-export default (DryadRepo: ComponentType<DryadRepoArgs>);
