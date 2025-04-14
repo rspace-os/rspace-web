e59253dd6ba5b6e642407f35ffd49360ea295fda
Robert Lamacraft <robert.lamacraft@researchspace.com>
2025-04-14 17:22:12 +0100

    Use DataGrid's built-in selection

--- src/main/webapp/ui/src/Inventory/Identifiers/IGSN/index.js
+++ src/main/webapp/ui/src/Inventory/Identifiers/IGSN/index.js
@@ -5,7 +5,6 @@ import Header from "../../components/Layout/Header";
 import Sidebar from "../../components/Layout/Sidebar";
 import IgsnManagementPage from "./IgsnManagementPage";
 import Box from "@mui/material/Box";
-import RsSet from "../../../util/set";
 import { type Identifier } from "../../useIdentifiers";
 
 /**
@@ -13,13 +12,18 @@ import { type Identifier } from "../../useIdentifiers";
  */
 export default function IGSN(): Node {
   const sidebarId = React.useId();
-  const [selectedIgsns, setSelectedIgsns] = React.useState(new RsSet<Identifier>());
+  const [selectedIgsns, setSelectedIgsns] = React.useState<
+    $ReadOnlyArray<Identifier>
+  >([]);
   return (
     <>
       <Header sidebarId={sidebarId} />
       <Box sx={{ display: "flex", height: "calc(100% - 48px)" }}>
         <Sidebar id={sidebarId} />
-        <IgsnManagementPage selectedIgsns={selectedIgsns} setSelectedIgsns={setSelectedIgsns}/>
+        <IgsnManagementPage
+          selectedIgsns={selectedIgsns}
+          setSelectedIgsns={setSelectedIgsns}
+        />
       </Box>
     </>
   );
