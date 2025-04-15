6b6aa71e2f8951aaa4b0d4af793ad59f9e20a986
Robert Lamacraft <robert.lamacraft@researchspace.com>
2025-04-15 16:41:10 +0100

    Hide sidebar button

--- src/main/webapp/ui/src/components/Inputs/TextField.js
+++ src/main/webapp/ui/src/components/Inputs/TextField.js
@@ -40,18 +40,59 @@ export default function TextField(props: TextFieldArgs): Node {
     }
     return <NoValue label={props.noValueLabel ?? "None"} />;
   }
+
+  const handleEditorInit = (editor) => {
+    editor.ui.registry.addAutocompleter('custom_autocompleter', {
+      ch: '/',
+      minChars: 0,
+      columns: 1,
+      fetch: (pattern) => {
+        const items = [
+          { text: 'Option 1', value: 'value1' },
+          { text: 'Option 2', value: 'value2' },
+        ];
+        return new Promise((resolve) => {
+          resolve(items.filter(item => item.text.indexOf(pattern) !== -1));
+        });
+      },
+      onAction: (autocompleteApi, rng, value) => {
+        editor.selection.setRng(rng);
+        editor.insertContent(value);
+        autocompleteApi.hide();
+      }
+    });
+    editor.ui.registry.addButton("pyrat", {
+      tooltip: "Link to PyRAT",
+      icon: "pyrat",
+      onAction() {
+        alert("open pyrat dialog");
+      },
+    });
+    editor.ui.registry.addMenuItem("optPyrat", {
+      text: "From PyRAT",
+      icon: "pyrat",
+      onAction() {
+        alert("open pyrat dialog");
+      },
+    });
+  };
+
   return (
     <Editor
       {...props}
       onChange={null}
       onEditorChange={handleEditorChange}
       init={{
-        menubar: false,
-        plugins: ["autolink", "lists", "link", "charmap", "autoresize"],
+        menu: {
+          insert: { title: 'Insert', items: 'link | charmap hr | optPyrat' },
+        },
+        plugins: ["autolink", "lists", "link", "charmap", "autoresize", "autocompleter", "pyrat"],
         toolbar:
           "undo redo blocks bold italic backcolor link \
-             bullist numlist removeformat",
+           bullist numlist removeformat \
+           pyrat",
         paste_block_drop: true,
+        setup: handleEditorInit,
       }}
     />
   );
