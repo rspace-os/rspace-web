2a1ddc438323a49a588dc7626e84199199e5750c
Robert Lamacraft <robert.lamacraft@researchspace.com>
2025-04-30 10:57:32 +0100

    down arrow moves focus

--- src/main/webapp/ui/src/components/useOneDimensionalRovingTabIndex.spec.tsx
+++ src/main/webapp/ui/src/components/useOneDimensionalRovingTabIndex.spec.tsx
@@ -13,11 +13,14 @@ const feature = test.extend<{
     }: {
       count: number;
     }) => Promise<void>;
+    "the roving list has focus": () => Promise<void>;
+    "the user presses the down arrow key": () => Promise<void>;
   };
   Then: {
     "there should be a button": () => Promise<void>;
     "the before button should gain focus": () => Promise<void>;
     "the after button should gain focus": () => Promise<void>;
+    "the second list item gains focus": () => Promise<void>;
   };
 }>({
   Given: async ({ mount }, use) => {
@@ -41,6 +44,22 @@ const feature = test.extend<{
           await page.keyboard.press("Tab");
         }
       },
+      "the roving list has focus": async () => {
+        let elementHasFocus = false;
+        while (!elementHasFocus) {
+          await page.keyboard.press("Tab");
+          elementHasFocus =
+            (await page.evaluate(
+              () =>
+                (
+                  document.activeElement?.parentNode as HTMLElement | null
+                )?.tagName.toLowerCase() === "li"
+            )) ?? false;
+        }
+      },
+      "the user presses the down arrow key": async () => {
+        await page.keyboard.press("ArrowDown");
+      },
     });
   },
   Then: async ({ page }, use) => {
@@ -55,6 +74,13 @@ const feature = test.extend<{
       "the after button should gain focus": async () => {
         const secondButton = page.getByRole("button").last();
         await expect(secondButton).toBeFocused();
+      },
+      "the second list item gains focus": async () => {
+        const secondListItem = page
+          .getByRole("listitem")
+          .nth(1)
+          .getByRole("button");
+        await expect(secondListItem).toBeFocused();
       },
     });
   },
@@ -74,4 +100,12 @@ test.describe("useOneDimensionalRovingTabIndex", () => {
       await Then["the after button should gain focus"]();
     }
   );
+  test.describe("The arrow keys traverse the roving list", () => {
+    feature("The down arrow moves the focus", async ({ Given, When, Then }) => {
+      await Given["the simple example component is rendered"]();
+      await When["the roving list has focus"]();
+      await When["the user presses the down arrow key"]();
+      await Then["the second list item gains focus"]();
+    });
+  });
 });
