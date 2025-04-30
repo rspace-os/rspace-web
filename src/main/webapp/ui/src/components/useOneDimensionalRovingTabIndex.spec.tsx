f783efe2aeb734c8945b8c11b696127e513d257c
Robert Lamacraft <robert.lamacraft@researchspace.com>
2025-04-30 10:32:08 +0100

    Tabbing over the roving list

--- src/main/webapp/ui/src/components/useOneDimensionalRovingTabIndex.spec.tsx
+++ src/main/webapp/ui/src/components/useOneDimensionalRovingTabIndex.spec.tsx
@@ -1,45 +1,77 @@
 import { test, expect } from "@playwright/experimental-ct-react";
 import React from "react";
-import { UseOneDimensionalRovingTabIndexStory } from "./useOneDimensionalRovingTabIndex.story";
+import { SimpleExample } from "./useOneDimensionalRovingTabIndex.story";
 
 const feature = test.extend<{
   Given: {
-    "the story is rendered": () => Promise<void>;
+    "the simple example component is rendered": () => Promise<void>;
   };
   Once: Record<string, never>;
-  When: Record<string, never>;
+  When: {
+    "the user presses the tab key {count} times": ({
+      count,
+    }: {
+      count: number;
+    }) => Promise<void>;
+  };
   Then: {
     "there should be a button": () => Promise<void>;
+    "the before button should gain focus": () => Promise<void>;
+    "the after button should gain focus": () => Promise<void>;
   };
 }>({
   Given: async ({ mount }, use) => {
     await use({
-      "the story is rendered": async () => {
-        await mount(<UseOneDimensionalRovingTabIndexStory />);
+      "the simple example component is rendered": async () => {
+        await mount(<SimpleExample />);
       },
     });
   },
   Once: async ({}, use) => {
     await use({});
   },
-  When: async ({}, use) => {
-    await use({});
+  When: async ({ page }, use) => {
+    await use({
+      "the user presses the tab key {count} times": async ({
+        count,
+      }: {
+        count: number;
+      }) => {
+        for (let i = 0; i < count; i++) {
+          await page.keyboard.press("Tab");
+        }
+      },
+    });
   },
   Then: async ({ page }, use) => {
     await use({
       "there should be a button": async () => {
         await expect(page.getByRole("button")).toBeVisible();
       },
+      "the before button should gain focus": async () => {
+        const firstButton = page.getByRole("button").first();
+        await expect(firstButton).toBeFocused();
+      },
+      "the after button should gain focus": async () => {
+        const secondButton = page.getByRole("button").last();
+        await expect(secondButton).toBeFocused();
+      },
     });
   },
 });
 
 test.describe("useOneDimensionalRovingTabIndex", () => {
+  feature("Tab focuses the before button", async ({ Given, When, Then }) => {
+    await Given["the simple example component is rendered"]();
+    await When["the user presses the tab key {count} times"]({ count: 1 });
+    await Then["the before button should gain focus"]();
+  });
   feature(
-    "when the story is rendered, then there should be a button",
-    async ({ Given, Then }) => {
-      await Given["the story is rendered"]();
-      await Then["there should be a button"]();
+    "Pressing tab thrice focusses the after button",
+    async ({ Given, When, Then }) => {
+      await Given["the simple example component is rendered"]();
+      await When["the user presses the tab key {count} times"]({ count: 3 });
+      await Then["the after button should gain focus"]();
     }
   );
 });
--- src/main/webapp/ui/src/components/useOneDimensionalRovingTabIndex.story.tsx
+++ src/main/webapp/ui/src/components/useOneDimensionalRovingTabIndex.story.tsx
@@ -1,6 +1,41 @@
 import React from "react";
 import useOneDimensionalRovingTabIndex from "./useOneDimensionalRovingTabIndex";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItem from "@mui/material/ListItem";
+import List from "@mui/material/List";
+import Button from "@mui/material/Button";
 
-export function UseOneDimensionalRovingTabIndexStory(): React.ReactNode {
-  return <button>click me</button>;
+/**
+ * A basic example of how to use useOneDimensionalRovingTabIndex
+ */
+export function SimpleExample(): React.ReactNode {
+  const { getTabIndex, getRef, eventHandlers } =
+    useOneDimensionalRovingTabIndex<HTMLDivElement>({ max: 3 });
+  return (
+    <>
+      <Button>Before the list</Button>
+      <List {...eventHandlers}>
+        <ListItem>
+          <ListItemButton
+            tabIndex={getTabIndex(0)}
+            ref={getRef(0)}
+            onClick={() => {}}
+          >
+            <ListItemText primary="One Thing" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem>
+          <ListItemButton
+            tabIndex={getTabIndex(1)}
+            ref={getRef(1)}
+            onClick={() => {}}
+          >
+            <ListItemText primary="Two Thing" />
+          </ListItemButton>
+        </ListItem>
+      </List>
+      <Button>After the list</Button>
+    </>
+  );
 }
