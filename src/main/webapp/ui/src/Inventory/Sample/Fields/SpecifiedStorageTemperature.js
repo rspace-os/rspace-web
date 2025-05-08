5c515fe2eaaece68a6edd157e378cb597a406c2d
Robert Lamacraft <robert.lamacraft@researchspace.com>
2025-05-08 09:40:06 +0100

    PRT-919 Don't prohibit the user from entering a hyphen when the field is empty
    The issue preventing negative temperature values is that the input handlers in
    SpecifiedStorageTemperature.js immediately parse input with parseInt() as soon
    as a character is typed. When a user types a hyphen as the first character,
    parseInt("-", 10) returns NaN rather than allowing the user to complete typing
    a negative number. For some reason we were applying parseInt twice, so this
    change removes the first one so that the string value of the field can be
    persisted allowing the user to type in hyphen and then a number.

--- src/main/webapp/ui/src/Inventory/Sample/Fields/SpecifiedStorageTemperature.js
+++ src/main/webapp/ui/src/Inventory/Sample/Fields/SpecifiedStorageTemperature.js
@@ -164,7 +164,7 @@ function SpecifiedStorageTemperature({
     });
   };
 
-  const handleMinFieldChange = (value: number) => {
+  const handleMinFieldChange = (value: sbtring) => {
     setMinField(value);
     const newMin = parseInt(value, DECIMAL);
     const newMax = !Number.isNaN(newMin) && newMin > max ? newMin : max;
@@ -179,7 +179,7 @@ function SpecifiedStorageTemperature({
     );
   };
 
-  const handleMaxFieldChange = (value: number) => {
+  const handleMaxFieldChange = (value: string) => {
     setMaxField(value);
     const newMax = parseInt(value, DECIMAL);
     const newMin = !Number.isNaN(newMax) && newMax < min ? newMax : min;
@@ -237,7 +237,7 @@ function SpecifiedStorageTemperature({
                           <NumberField
                             value={minField}
                             onChange={({ target: { value: newValue } }) => {
-                              handleMinFieldChange(parseInt(newValue, 10));
+                              handleMinFieldChange(newValue);
                             }}
                             variant="outlined"
                             size="small"
@@ -259,7 +259,7 @@ function SpecifiedStorageTemperature({
                           <NumberField
                             value={maxField}
                             onChange={({ target: { value } }) => {
-                              handleMaxFieldChange(parseInt(value, 10));
+                              handleMaxFieldChange(value);
                             }}
                             variant="outlined"
                             size="small"
