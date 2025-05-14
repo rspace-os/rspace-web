64aeb623d0bcb39cdeb5c598250700d2aa3231e1
Robert Lamacraft <robert.lamacraft@researchspace.com>
2025-05-14 09:19:19 +0100

    Add calendar button to messaging page

--- src/main/webapp/scripts/pages/workspace/calendarDialog.js
+++ src/main/webapp/scripts/pages/workspace/calendarDialog.js
@@ -7,6 +7,7 @@ function clearCalendarDialogAttachments() {
 }
 
 function initCreateCalendarEntryDlg() {
+  console.debug("initCreateCalendarEntryDlg");
   if (!RS.useBootstrapModals) {
      $('#createCalendarEntryDlg').dialog({
         modal: true,
