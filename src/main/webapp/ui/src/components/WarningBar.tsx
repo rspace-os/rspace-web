da0590d15ccf7b2acc287bda5e2bf046f4934c88
Robert Lamacraft <robert.lamacraft@researchspace.com>
2025-05-15 14:09:31 +0100

    WarningBar

--- src/main/webapp/ui/src/components/WarningBar.tsx
+++ src/main/webapp/ui/src/components/WarningBar.tsx
@@ -1,16 +1,14 @@
-//@flow
-
-import React, { type Node } from "react";
+import React from "react";
 import WarningIcon from "@mui/icons-material/Warning";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 
-/*
+/**
  * This component is for showing a warning label just above a dialog's submit
  * button that there are unsaved changes to the contents of the dialog that
  * will be lost if the user closes the dialog without submitting.
  */
-export default function WarningBar(): Node {
+export default function WarningBar(): React.ReactNode {
   return (
     <Grid
       container
