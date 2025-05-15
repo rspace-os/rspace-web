9499fc738da14474660998dc3d3b7bc1c27006da
Robert Lamacraft <robert.lamacraft@researchspace.com>
2025-05-15 10:38:41 +0100

    Restore unintended changes

--- src/main/webapp/scripts/pages/workspace/calendarDialog.js
+++ src/main/webapp/scripts/pages/workspace/calendarDialog.js
@@ -7,7 +7,6 @@ function clearCalendarDialogAttachments() {
 }
 
 function initCreateCalendarEntryDlg() {
-  console.debug("initCreateCalendarEntryDlg");
   if (!RS.useBootstrapModals) {
      $('#createCalendarEntryDlg').dialog({
         modal: true,
