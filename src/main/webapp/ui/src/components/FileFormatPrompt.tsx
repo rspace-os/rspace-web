e0a793f4834b1cd7e09e69b803c94482a943369d
Robert Lamacraft <robert.lamacraft@researchspace.com>
2025-05-15 11:59:08 +0100

    FileFormatPrompt

--- src/main/webapp/ui/src/components/FileFormatPrompt.tsx
+++ src/main/webapp/ui/src/components/FileFormatPrompt.tsx
@@ -1,6 +1,4 @@
-//@flow
-
-import React, { type Node } from "react";
+import React from "react";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
@@ -9,39 +7,43 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import CloseIcon from "@mui/icons-material/Close";
 import IconButton from "@mui/material/IconButton";
+import styled from "@emotion/styled";
 
-const styles = {
-  closeButton: {
-    position: "absolute",
-    right: 0,
-    top: 0,
-    width: "auto",
-  },
+const CloseButton = styled(IconButton)`
+  position: absolute;
+  right: 0;
+  top: 0;
+  width: auto;
+`;
+
+const StyledDialogContent = styled(DialogContent)`
+  overscroll-behavior: contain;
+`;
+
+type ImageEditorDialogArgs = {
+  jpegSize: string;
+  pngSize: string;
+  saveAs: (extension: "png" | "jpeg") => void;
+  closePrompt: () => void;
+  open: boolean;
 };
 
-type ImageEditorDialogArgs = {|
-  jpegSize: string,
-  pngSize: string,
-  saveAs: ("png" | "jpeg") => void,
-  closePrompt: () => void,
-  open: boolean,
-|};
-
-export default function ImageEditorDialog(props: ImageEditorDialogArgs): Node {
+export default function ImageEditorDialog(
+  props: ImageEditorDialogArgs
+): React.ReactNode {
   return (
     <Dialog open={props.open}>
       <DialogTitle>
         Choose image format
-        <IconButton
+        <CloseButton
           aria-label="Close"
           data-test-id="closeModal"
-          style={styles.closeButton}
           onClick={props.closePrompt}
         >
           <CloseIcon />
-        </IconButton>
+        </CloseButton>
       </DialogTitle>
-      <DialogContent style={{ overscrollBehavior: "contain" }}>
+      <StyledDialogContent>
         <Typography variant="subtitle1" gutterBottom>
           The image you edited was in a JPEG format.
           <ul>
@@ -52,11 +54,11 @@ export default function ImageEditorDialog(props: ImageEditorDialogArgs): Node {
             <li>If you save as PNG, the image size might increase.</li>
           </ul>
         </Typography>
-      </DialogContent>
+      </StyledDialogContent>
       <DialogActions>
         <Button
           onClick={() => props.saveAs("png")}
-          style={{ color: "grey" }}
+          sx={{ color: "grey " }}
           data-test-id="save-as-png"
         >
           {`Save as PNG (${props.pngSize})`}
