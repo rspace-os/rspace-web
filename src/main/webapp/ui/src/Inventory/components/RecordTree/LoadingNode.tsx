e58a5b6c182160e9ee93b96e28f9f32f35a484af
Robert Lamacraft <robert.lamacraft@researchspace.com>
2025-05-21 11:20:32 +0100

    LoadingNode

--- src/main/webapp/ui/src/Inventory/components/RecordTree/LoadingNode.tsx
+++ src/main/webapp/ui/src/Inventory/components/RecordTree/LoadingNode.tsx
@@ -1,12 +1,10 @@
-// @flow
-
-import React, { type ComponentType } from "react";
+import React from "react";
 import Skeleton from "@mui/material/Skeleton";
 import { TreeItem } from "@mui/x-tree-view/TreeItem";
 
 let nextUniqueId = 0;
 
-const LoadingNode: ComponentType<{}> = () => (
+const LoadingNode = () => (
   <TreeItem
     label={<Skeleton variant="text" animation="wave" height={32} />}
     itemId={`${nextUniqueId++}`}
