8828b64445c245fc0a0f329386aef43e40c58f84
Robert Lamacraft <robert.lamacraft@researchspace.com>
2025-05-21 16:00:26 +0100

    Documentation

--- src/main/webapp/ui/src/stores/definitions/HasLocation.ts
+++ src/main/webapp/ui/src/stores/definitions/HasLocation.ts
@@ -3,23 +3,31 @@ import { InventoryRecord } from "./InventoryRecord";
 import { Person } from "./Person";
 
 /**
- * This is used to mark implementations of the HasLocation interface so that
+ * This is used to mark implementations of the HasLocation interface so that at
  * runtime we can filter collections of InventoryRecord, operating just on those
  * that have a location.
  */
 export const HasLocationMarker = Symbol("HasLocation");
 
+/**
+ * There are no fields associated with records that have a location that are
+ * editable. To edit the location of an item, the user must perform a move
+ * action rather than directly edit the location property.
+ */
 export type HasLocationEditableFields = object;
 
+/**
+ * When a record has a location, that location can be considered a field that
+ * cannot be edited in the UI. To change the location, the user must perform a
+ * move operation rather than edit the location property directly.
+ */
 export type HasLocationUneditableFields = {
   location: InventoryRecord;
 };
 
 /**
  * Inventory records that model items that physically exist and thus have a
- * real-world location MUST implement this interface. To avoid duplicating
- * logic, each class can delegate most of the logic concerning this location
- * state to the HasLocationCapability model class.
+ * real-world location MUST implement this interface.
  */
 export interface HasLocation {
   [HasLocationMarker]: true;
@@ -36,7 +44,7 @@ export interface HasLocation {
    * Because items which have a location have a parent container, and containers
    * are themselves items which have a location, we end up with a tree of
    * containers. At the root of the tree are containers which do not reside
-   * within other containers; these include workbenches, and any other contaienrs
+   * within other containers; these include workbenches, and any other containers
    * that the users of the system have designated as root containers, perhaps
    * cupboards, freezers, or whole rooms. This property gets a reference to the
    * root container. It will be null if this item is itself a root container.
@@ -58,19 +66,19 @@ export interface HasLocation {
   /*
    * Determines whether the Inventory record is directly on a workbench,
    * which is to say the immediate parent container is a workbench. If this is
-   * true, then it implies that `isOnWorkbench` is always true.
+   * true, then it implies that `isOnWorkbench` is also true.
    */
   readonly isDirectlyOnWorkbench: boolean;
 
   /*
-   * Not only is this item transitively on a workbench, but the workbench that
-   * it is on is owned by the specified user.
+   * Not only is this item on a workbench, but the workbench that it is on is
+   * owned by the specified user.
    */
   isOnWorkbenchOfUser(user: Person): boolean;
 
   /*
    * Not only is this item directly on a workbench, but that workbench that it
-   * is on it owned by the specified user.
+   * is on is owned by the specified user.
    */
   isDirectlyOnWorkbenchOfUser(user: Person): boolean;
 
