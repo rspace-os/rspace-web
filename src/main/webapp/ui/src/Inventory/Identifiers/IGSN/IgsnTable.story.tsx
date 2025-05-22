99b5d9140148ace8a770ee441ec5950e583cfbcd
Robert Lamacraft <robert.lamacraft@researchspace.com>
2025-05-22 11:01:17 +0100

    Make selection work

--- src/main/webapp/ui/src/Inventory/Identifiers/IGSN/IgsnTable.story.tsx
+++ src/main/webapp/ui/src/Inventory/Identifiers/IGSN/IgsnTable.story.tsx
@@ -5,7 +5,7 @@ import materialTheme from "../../../theme";
 import IgsnTable from "./IgsnTable";
 import { type Identifier } from "../../useIdentifiers";
 
-export default function IgsnTableStory() {
+export function SimpleIgsnTable() {
   const [selectedIgsns, setSelectedIgsns] = React.useState<
     ReadonlyArray<Identifier>
   >([]);
@@ -16,6 +16,30 @@ export default function IgsnTableStory() {
         <IgsnTable
           selectedIgsns={selectedIgsns}
           setSelectedIgsns={setSelectedIgsns}
+        />
+        <h2>Selected IGSNs</h2>
+        <ul aria-label="selected IGSNs">
+          {selectedIgsns.map((igsn) => (
+            <li key={igsn.id}>{igsn.doi}</li>
+          ))}
+        </ul>
+      </ThemeProvider>
+    </StyledEngineProvider>
+  );
+}
+
+export function SingularSelectionIgsnTable() {
+  const [selectedIgsns, setSelectedIgsns] = React.useState<
+    ReadonlyArray<Identifier>
+  >([]);
+  return (
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={materialTheme}>
+        <h1>IGSN Table</h1>
+        <IgsnTable
+          selectedIgsns={selectedIgsns}
+          setSelectedIgsns={setSelectedIgsns}
+          disableMultipleRowSelection
         />
         <h2>Selected IGSNs</h2>
         <ul aria-label="selected IGSNs">
