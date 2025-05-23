b73f2c3fd713b6b8b8c5e96bf07636ff81e1f1c0
Robert Lamacraft <robert.lamacraft@researchspace.com>
2025-05-23 14:41:01 +0100

    Back out "Remove unimplemented ReferenceField"
    This backs out commit 2dc7896f435bfe9ec5aec84dd869ad4c05f23754.

---
+++ src/main/webapp/ui/src/components/Inputs/ReferenceField.js
@@ -0,0 +1,8 @@
+//@flow
+import React, { type Node } from "react";
+
+export type ReferenceFieldArgs = {||};
+
+export default function ReferenceField(): Node {
+  return <i>Not yet supported.</i>;
+}
