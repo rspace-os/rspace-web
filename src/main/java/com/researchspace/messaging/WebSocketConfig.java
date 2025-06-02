fa16550e2e568fa2ed2474d69c6cff7b67ea7686
rs-fraser <fraser.macinnes>
2025-06-02 15:46:22 +0100

    remove path that's not used

--- src/main/java/com/researchspace/messaging/WebSocketConfig.java
+++ src/main/java/com/researchspace/messaging/WebSocketConfig.java
@@ -13,7 +13,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   @Override
   public void configureMessageBroker(MessageBrokerRegistry config) {
     config.enableSimpleBroker("/topic");
-    config.setApplicationDestinationPrefixes("/app");
   }
 
   @Override
