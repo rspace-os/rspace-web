41212dd1a2804cfb9b02451b38e850be81d01413
Robert Lamacraft <robert.lamacraft@researchspace.com>
2025-06-04 14:51:52 +0100

    Migrate ReferenceField.js to TypeScript

--- src/main/webapp/ui/src/components/Inputs/ReferenceField.js
+++
@@ -1,8 +0,0 @@
-//@flow
-import React, { type Node } from "react";
-
-export type ReferenceFieldArgs = {||};
-
-export default function ReferenceField(): Node {
-  return <i>Not yet supported.</i>;
-}
