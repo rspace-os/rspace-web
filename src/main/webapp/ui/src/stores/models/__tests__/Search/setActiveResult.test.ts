23941b938bc6468c621e3daae8def4b0b73f362d
Robert Lamacraft <robert.lamacraft@researchspace.com>
2025-06-05 16:38:13 +0100

    setActiveResult.test

--- src/main/webapp/ui/src/stores/models/__tests__/Search/setActiveResult.test.ts
+++ src/main/webapp/ui/src/stores/models/__tests__/Search/setActiveResult.test.ts
@@ -1,33 +1,33 @@
 /*
  * @jest-environment jsdom
  */
-//@flow
 /* eslint-env jest */
 import "@testing-library/jest-dom";
 import Search from "../../Search";
 import { makeMockContainer } from "../ContainerModel/mocking";
 import LocationModel from "../../LocationModel";
 import { mockFactory } from "../../../definitions/__tests__/Factory/mocking";
-import { type InventoryRecord } from "../../../definitions/InventoryRecord";
 
 jest.mock("../../../../common/InvApiService", () => {}); // break import cycle
 
 describe("setActiveResult", () => {
   describe("callback", () => {
     test("should only be called after fetchAdditionalInfo has completed.", async () => {
-      const callback = jest.fn<[?InventoryRecord], void>((c) => {
-        // $FlowExpectedError[prop-missing] setActiveResult is passed a container
-        // $FlowExpectedError[incompatible-use]
+      const callback = jest.fn().mockImplementation((c) => {
         expect(c.locations.length).toBe(1);
       });
+
       const search = new Search({
         callbacks: {
           setActiveResult: callback,
         },
         factory: mockFactory(),
       });
+
       const container = makeMockContainer();
-      jest.spyOn(container, "fetchAdditionalInfo").mockImplementation(() => {
+      // Mock the fetchAdditionalInfo method
+      const mockFetch = jest.spyOn(container, "fetchAdditionalInfo");
+      mockFetch.mockImplementation(() => {
         container.locations = [
           new LocationModel({
             id: null,
@@ -44,6 +44,15 @@ describe("setActiveResult", () => {
       });
 
       await search.setActiveResult(container);
+
+      // Verify the callback was called
+      expect(callback).toHaveBeenCalled();
+
+      // Verify the mock implementation was called
+      expect(mockFetch).toHaveBeenCalled();
+
+      // Use non-null assertion since we know we set the locations in the mock
+      expect(container.locations!.length).toBe(1);
     });
   });
 });
