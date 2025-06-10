e6fdeaafbaae253ecda58a4589124ae177d59cb9
rs-fraser <fraser.macinnes>
2025-06-10 17:11:46 +0100

    wip: pubchem backend starter

--- src/main/webapp/ui/src/Toolbar/Workspace/SimpleSearch/ChemicalSearcher.js
+++ src/main/webapp/ui/src/Toolbar/Workspace/SimpleSearch/ChemicalSearcher.js
@@ -86,6 +86,7 @@ const ChemicalSearcher = ({ isOpen, onClose }) => {
   const handleInsert = (ketcher) => {
     setShowKetcherDialog(false);
     ketcher.getSmiles().then((smiles) => {
+      console.log("smiles", smiles);
       setSearchSmiles(smiles);
       fetchData(0, smiles);
     });
