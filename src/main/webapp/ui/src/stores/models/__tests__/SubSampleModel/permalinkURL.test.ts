2231886f9350598e0d39496eab9c657127045ef9
Robert Lamacraft <robert.lamacraft@researchspace.com>
2025-06-20 15:06:27 +0100

    Migrated permalinkURL.test.js to TypeScript

--- src/main/webapp/ui/src/stores/models/__tests__/SubSampleModel/permalinkURL.test.ts
+++ src/main/webapp/ui/src/stores/models/__tests__/SubSampleModel/permalinkURL.test.ts
@@ -1,12 +1,11 @@
 /*
  * @jest-environment jsdom
  */
-//@flow
 /* eslint-env jest */
 import "@testing-library/jest-dom";
 import { makeMockSubSample } from "./mocking";
 
-jest.mock("../../../../common/InvApiService", () => {});
+jest.mock("../../../../common/InvApiService", () => ({}));
 jest.mock("../../../../stores/stores/RootStore", () => () => ({
   unitStore: {
     getUnit: () => ({ label: "ml" }),
