29265d063f0f7e37059e1053fd94984554d2a092
Robert Lamacraft <robert.lamacraft@researchspace.com>
2025-06-20 10:46:20 +0100

    Migrate src/Inventory/Import/Fields/TableCell.js to TypeScript

---
+++ src/main/webapp/ui/src/Inventory/Import/Fields/TableCell.tsx
@@ -0,0 +1,20 @@
+import CustomTableCell from "../../Search/components/TableCell";
+import React from "react";
+import { withStyles } from "Styles";
+
+type TableCellArgs = React.ComponentProps<typeof CustomTableCell> & {
+  nopadding: boolean;
+  borderless?: boolean;
+};
+
+const TableCell = withStyles<TableCellArgs, { root: string }>(
+  (theme, { nopadding, borderless }) => ({
+    root: {
+      padding: nopadding ? 0 : theme.spacing(0.5),
+      borderBottom: borderless ? "unset" : theme.borders.table,
+    },
+  })
+)(({ nopadding, borderless, ...rest }) => <CustomTableCell {...rest} />); // eslint-disable-line no-unused-vars
+
+TableCell.displayName = "TableCell";
+export default TableCell;
