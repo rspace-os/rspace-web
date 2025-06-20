84188109597793e73df6c47c78a2d16403fc9a9d
Robert Lamacraft <robert.lamacraft@researchspace.com>
2025-06-20 09:55:58 +0100

    Migrate FieldTypeMenuItemOpenIcon.js to TypeScript

--- src/main/webapp/ui/src/Inventory/Import/Fields/FieldTypeMenuItemOpenIcon.tsx
+++ src/main/webapp/ui/src/Inventory/Import/Fields/FieldTypeMenuItemOpenIcon.tsx
@@ -1,13 +1,11 @@
-//@flow
-
-import React, { type Node } from "react";
+import React from "react";
 import ListIcon from "@mui/icons-material/List";
 import CloseIcon from "@mui/icons-material/Close";
 
 export default function FieldTypeMenuItemOpenIcon({
   open,
 }: {
-  open: boolean,
-}): Node {
+  open: boolean;
+}): React.ReactNode {
   return open ? <CloseIcon /> : <ListIcon />;
 }
