b05d975b7341f2f4cdac502c3c75e352c6ec3cf5
Robert Lamacraft <robert.lamacraft@researchspace.com>
2025-06-23 17:14:38 +0100

    Migrate src/Inventory/Identifiers/IGSN/index.js to TypeScript

--- src/main/webapp/ui/src/Inventory/Identifiers/IGSN/index.tsx
+++ src/main/webapp/ui/src/Inventory/Identifiers/IGSN/index.tsx
@@ -1,18 +1,18 @@
-// @flow
-
-import React, { type Node } from "react";
+import React from "react";
 import Header from "../../components/Layout/Header";
 import Sidebar from "../../components/Layout/Sidebar";
 import IgsnManagementPage from "./IgsnManagementPage";
 import Box from "@mui/material/Box";
-import { type Identifier } from "../../useIdentifiers";
-import { IdentifiersRefreshProvider } from "../../useIdentifiers";
+import {
+  type Identifier,
+  IdentifiersRefreshProvider,
+} from "../../useIdentifiers";
 import RsSet from "../../../util/set";
 
 /**
  * This is the page where researchers can manage their IGSNs.
  */
-export default function IGSN(): Node {
+export default function IGSN(): React.ReactNode {
   const sidebarId = React.useId();
   const [selectedIgsns, setSelectedIgsns] = React.useState<RsSet<Identifier>>(
     new RsSet([])
