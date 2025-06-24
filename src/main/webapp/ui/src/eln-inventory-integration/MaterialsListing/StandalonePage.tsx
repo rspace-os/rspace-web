f2f38ed8443f9158493a390dd44eba6a833d6304
Robert Lamacraft <robert.lamacraft@researchspace.com>
2025-06-24 16:21:10 +0100

    MaterialsListing/StandalonePage

--- src/main/webapp/ui/src/eln-inventory-integration/MaterialsListing/StandalonePage.tsx
+++ src/main/webapp/ui/src/eln-inventory-integration/MaterialsListing/StandalonePage.tsx
@@ -1,16 +1,15 @@
-//@flow
-
-import React, { useEffect, type Node } from "react";
+import React, { useEffect } from "react";
 import MaterialsDialog from "./MaterialsDialog";
 import { useParams } from "react-router-dom";
 import useStores from "../../stores/use-stores";
 import AlwaysNewWindowNavigationContext from "../../components/AlwaysNewWindowNavigationContext";
 
-export default function StandaloneListOfMaterialsPage(): Node {
+export default function StandaloneListOfMaterialsPage(): React.ReactNode {
   const { lomId } = useParams();
   const { materialsStore } = useStores();
 
   useEffect(() => {
+    if (lomId === undefined) throw new Error("lomId is undefined");
     void materialsStore.getMaterialsListing(parseInt(lomId, 10));
   }, [lomId]);
 
