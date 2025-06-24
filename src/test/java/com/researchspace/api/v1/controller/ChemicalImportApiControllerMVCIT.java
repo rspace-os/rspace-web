39ed9c23589366a3c98d277c26bb9cc629e88f26
rs-fraser <fraser.macinnes>
2025-06-24 16:10:17 +0100

    add integration sanity test to ensure connection can be made to pubchem and reponse is as expected

--- src/test/java/com/researchspace/api/v1/controller/ChemicalImportApiControllerMVCIT.java
+++ src/test/java/com/researchspace/api/v1/controller/ChemicalImportApiControllerMVCIT.java
@@ -1,13 +1,23 @@
 package com.researchspace.api.v1.controller;
 
+import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.researchspace.model.User;
+import com.researchspace.model.dtos.chemistry.ChemicalImportSearchResult;
+import com.researchspace.model.dtos.chemistry.ChemicalImportSearchType;
+import com.researchspace.model.dtos.chemistry.ChemicalSearchRequest;
+import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MvcResult;
 
+/**
+ * Sanity tests to verify connection to PubChem and response format.
+ */
 @WebAppConfiguration
 public class ChemicalImportApiControllerMVCIT extends API_MVC_TestBase {
 
@@ -21,12 +31,44 @@ public class ChemicalImportApiControllerMVCIT extends API_MVC_TestBase {
     apiKey = createNewApiKeyForUser(user);
   }
 
+  //  @RunIfSystemPropertyDefined("nightly")
   @Test
   public void testSearchByName() throws Exception {
+    ChemicalSearchRequest body =
+        new ChemicalSearchRequest(ChemicalImportSearchType.NAME, "aspirin");
     MvcResult result =
         mockMvc
-            .perform(createBuilderForPost(API_VERSION.ONE, apiKey, "chemical/search", user))
+            .perform(createBuilderForPostWithJSONBody(apiKey, "chemical/search", user, body))
             .andExpect(status().isOk())
             .andReturn();
+
+    List<ChemicalImportSearchResult> expected =
+        List.of(
+            new ChemicalImportSearchResult(
+                "2-acetoxybenzoic acid",
+                "https://pubchem.ncbi.nlm.nih.gov/image/imgsrv.fcgi?cid=2244&t=l",
+                "CC(=O)OC1=CC=CC=C1C(=O)O",
+                "C9H8O4",
+                "2244",
+                "https://pubchem.ncbi.nlm.nih.gov/compound/2244"));
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    List<ChemicalImportSearchResult> actual =
+        objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {});
+
+    assertEquals(expected, actual);
+  }
+
+  //  @RunIfSystemPropertyDefined("nightly")
+  @Test
+  public void testImportByCID() throws Exception {
+    List<String> body = List.of("2244", "2246");
+    MvcResult result =
+        mockMvc
+            .perform(createBuilderForPostWithJSONBody(apiKey, "chemical/import", user, body))
+            .andExpect(status().isCreated())
+            .andReturn();
+
+    assertEquals("", result.getResponse().getContentAsString());
   }
 }
