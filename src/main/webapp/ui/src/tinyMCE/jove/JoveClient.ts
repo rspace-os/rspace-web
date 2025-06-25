6830187011e5c487f67882128c93044b36965d32
Robert Lamacraft <robert.lamacraft@researchspace.com>
2025-06-25 13:25:55 +0100

    JoveClient

--- src/main/webapp/ui/src/tinyMCE/jove/JoveClient.ts
+++ src/main/webapp/ui/src/tinyMCE/jove/JoveClient.ts
@@ -1,35 +1,33 @@
-// @flow
-
 import axios, { type AxiosPromise } from "@/common/axios";
 import { SearchParam } from "./Enums";
 
 export type ArticleId = any;
 
-export type Article = {|
-  id: ArticleId,
-  hasvideo: boolean,
-  section: string,
-  thumbnail: string,
-  title: string,
-  url: string,
-  embed_url: string,
-|};
+export type Article = {
+  id: ArticleId;
+  hasvideo: boolean;
+  section: string;
+  thumbnail: string;
+  title: string;
+  url: string;
+  embed_url: string;
+};
 
-export type JoveSearchResult = {|
-  articlelist: Array<Article>,
-  countall: number,
-  pagecount: number,
-  pagesize: number,
-|};
+export type JoveSearchResult = {
+  articlelist: Array<Article>;
+  countall: number;
+  pagecount: number;
+  pagesize: number;
+};
 
 export const search = async (
-  searchParam: $Values<typeof SearchParam>,
+  searchParam: (typeof SearchParam)[keyof typeof SearchParam],
   query: string,
   page: number,
   pageSize: number
-): Promise<?{|
-  data: JoveSearchResult,
-|}> => {
+): Promise<{
+  data: JoveSearchResult;
+} | null> => {
   let request = {};
   if (searchParam === SearchParam.queryString) {
     request = { queryString: query, pageNumber: page, pageSize: pageSize };
@@ -38,25 +36,25 @@ export const search = async (
   } else if (searchParam === SearchParam.institution) {
     request = { institution: query, pageNumber: page, pageSize: pageSize };
   }
+  // @ts-expect-error global
   RS.trackEvent("JoveSearchRequest", request); //eslint-disable-line
   const result = await makeSearchRequest(request);
   if (result.data.joveSearchResult) {
     return { data: result.data.joveSearchResult };
-  } else {
-    return null;
   }
+  return null;
 };
 
 export const getArticle = async (
   articleId: ArticleId
-): Promise<{| data: Article |}> => {
+): Promise<{ data: Article }> => {
   const article = await makeGetArticleRequest(articleId);
   return { data: article.data.joveArticle };
 };
 
 const makeSearchRequest = (
-  joveSearchRequest: mixed
-): AxiosPromise<mixed, {| joveSearchResult: JoveSearchResult |}> => {
+  joveSearchRequest: unknown
+): AxiosPromise<{ joveSearchResult: JoveSearchResult }> => {
   return axios.post("/apps/jove/search", joveSearchRequest);
 };
 
