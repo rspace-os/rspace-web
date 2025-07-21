5e8a241df1ebf6c834e54e9215ca77fa28d62227
Robert Lamacraft <robert.lamacraft@researchspace.com>
2025-07-21 08:55:15 +0100

    partial solution

--- src/main/webapp/ui/src/Inventory/Subsample/Fields/Notes/NewNote.tsx
+++ src/main/webapp/ui/src/Inventory/Subsample/Fields/Notes/NewNote.tsx
@@ -18,12 +18,27 @@ type NewNoteArgs = {
 function NewNote({ record, onErrorStateChange }: NewNoteArgs): React.ReactNode {
   const [note, setNote] = useState("");
 
+  /*
+   * TextField's onChange handler is called both when the user types and when
+   * the `value` prop changes. `handleChange` needs to call `onErrorStateChange`
+   * so that if the user types more characters than is allowed then we propagate
+   * on error state up to the parent section of the form. However, when
+   * `createNote` resets the TextField after succesfully saving the new note,
+   * `handleChange` will now be called with a value of the empty string. This is
+   * technically an error, but we don't want the parent form section to be in an
+   * error state when the user has just created a note, so we track whether this
+   * is the initial state of the TextField with `initial` and only call
+   * `onErrorStateChange` if it is not the initial state.
+   */
+  const [initial, setInitial] = useState(true);
+
   const handleChange = ({
     target: { value },
   }: {
     target: { name: string; value: string };
   }) => {
     setNote(value);
+    setInitial(false);
     if (value === "") {
       // when editing, unsetting the dirty flag could result in other changes
       // being not savable
@@ -31,26 +46,33 @@ function NewNote({ record, onErrorStateChange }: NewNoteArgs): React.ReactNode {
     } else {
       record.setDirtyFlag();
     }
-    onErrorStateChange(validate().isError);
+    console.debug("calling onErrorStateChange", {
+      value,
+      initial,
+      isError: validate(value).isError,
+      errors: validate(value).orElseGet((e) => e),
+    });
+    onErrorStateChange(!initial && validate(value).isError);
   };
 
   const createNote = () => {
     void record.createNote({ content: note });
     setNote("");
+    setInitial(true);
   };
 
   useEffect(() => {
     setNote("");
   }, [record]);
 
-  function validate() {
+  function validate(value: string) {
     if (!record.isFieldEditable("notes")) {
       return IsInvalid("Notes are not editable");
     }
-    if (note === "") {
+    if (value === "") {
       return IsInvalid("Note cannot be empty.");
     }
-    if (note.length > 2000) {
+    if (value.length > 2000) {
       return IsInvalid("Note cannot exceed 2000 characters.");
     }
     return IsValid();
@@ -77,7 +99,7 @@ function NewNote({ record, onErrorStateChange }: NewNoteArgs): React.ReactNode {
           deleted.
         </Typography>
         <ValidatingSubmitButton
-          validationResult={validate()}
+          validationResult={validate(note)}
           onClick={createNote}
           loading={false}
         >
