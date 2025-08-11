e5b9babc7d7389e8de1228bf00a76d129c1c2cf7
rs-fraser <141634676+rs-fraser@users.noreply.github.com>
2025-08-11 11:40:53 +0100

    formattign

--- src/main/java/com/researchspace/service/chemistry/ChemistryClient.java
+++ src/main/java/com/researchspace/service/chemistry/ChemistryClient.java
@@ -13,7 +13,6 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
