6c2eaa3ced4002e14eae912d48593f0f21c5857f
Nico Ferrante <160590956+rs-nicof@users.noreply.github.com>
2025-09-09 17:08:09 +0100

    RSDEV-535: Provide links to DMP service in Gallery info panel (#464)
    Adding more attributes into `RecordInformation`object in order to de shown by the UI

--- src/main/java/com/researchspace/service/impl/RecordManagerImpl.java
+++ src/main/java/com/researchspace/service/impl/RecordManagerImpl.java
@@ -1,7 +1,11 @@
 package com.researchspace.service.impl;
 
+import static com.researchspace.core.util.MediaUtils.DMP_MEDIA_FLDER_NAME;
+import static com.researchspace.core.util.MediaUtils.DOCUMENT_MEDIA_FLDER_NAME;
+import static com.researchspace.core.util.MediaUtils.MISC_MEDIA_FLDER_NAME;
 import static com.researchspace.model.comms.NotificationType.NOTIFICATION_DOCUMENT_EDITED;
 import static com.researchspace.model.record.BaseRecord.DEFAULT_VARCHAR_LENGTH;
+import static com.researchspace.model.record.Folder.EXPORTS_FOLDER_NAME;
 import static java.lang.String.format;
 import static org.apache.commons.lang.StringUtils.abbreviate;
 import static org.apache.commons.lang.StringUtils.trim;
@@ -13,6 +17,7 @@ import com.researchspace.core.util.JacksonUtil;
 import com.researchspace.core.util.SearchResultsImpl;
 import com.researchspace.core.util.TransformerUtils;
 import com.researchspace.dao.AuditDao;
+import com.researchspace.dao.DMPDao;
 import com.researchspace.dao.EcatImageDao;
 import com.researchspace.dao.FieldDao;
 import com.researchspace.dao.FolderDao;
@@ -39,6 +44,7 @@ import com.researchspace.model.audit.AuditedEntity;
 import com.researchspace.model.core.GlobalIdPrefix;
 import com.researchspace.model.core.GlobalIdentifier;
 import com.researchspace.model.core.RecordType;
+import com.researchspace.model.dmps.DMPUser;
 import com.researchspace.model.dtos.GalleryFilterCriteria;
 import com.researchspace.model.dtos.WorkspaceFilters;
 import com.researchspace.model.dtos.WorkspaceListingConfig;
@@ -62,6 +68,7 @@ import com.researchspace.model.record.IllegalAddChildOperation;
 import com.researchspace.model.record.ImportOverride;
 import com.researchspace.model.record.RSForm;
 import com.researchspace.model.record.Record;
+import com.researchspace.model.record.RecordInformation;
 import com.researchspace.model.record.RecordToFolder;
 import com.researchspace.model.record.Snippet;
 import com.researchspace.model.record.StructuredDocument;
@@ -122,6 +129,7 @@ public class RecordManagerImpl implements RecordManager {
   private @Autowired FieldDao fieldDao;
   private @Autowired FolderDao folderDao;
   private @Autowired EcatImageDao imageDao;
+  private @Autowired DMPDao dmpDao;
   private @Autowired FormUsageDao formUsageDao;
   private @Autowired RecordEditorTracker tracker;
   private @Autowired FieldLinksEntitiesSynchronizer fieldContentSynchroniser;
@@ -1384,5 +1392,44 @@ public class RecordManagerImpl implements RecordManager {
       User user, Folder folderOrNotebook) {
     return folderOrNotebook.isSharedFolder()
         || isSharedNotebookWithoutCreatePermission(user, folderOrNotebook);
+  }
+
+  @Override
+  public RecordInformation decorateRecordInfo(
+      RecordInformation recordInfo,
+      User user,
+      Folder parentFolder,
+      boolean isOnRoot,
+      BaseRecord baseRecord,
+      String mediaType) {
+    if (baseRecord instanceof EcatDocumentFile) {
+      EcatDocumentFile doc = (EcatDocumentFile) baseRecord;
+      recordInfo.addType(getEcatDocumentFileType(mediaType, doc.getDocumentType()));
+    }
+
+    if ("DMPs".equals(mediaType)) {
+      DMPUser dmpSaved = dmpDao.findDMPsByFile(user, baseRecord.getId()).get(0);
+      recordInfo.setDmpLink(dmpSaved.getDmpLink());
+      recordInfo.setDoiLink(dmpSaved.getDoiLink());
+      recordInfo.setDmpSource(dmpSaved.getSource());
+    }
+
+    recordInfo.setParentId(parentFolder.getId());
+    recordInfo.setOnRoot(isOnRoot);
+
+    return recordInfo;
+  }
+
+  private String getEcatDocumentFileType(String mediatype, String documentType) {
+    if (EXPORTS_FOLDER_NAME.equalsIgnoreCase(mediatype)) {
+      return EXPORTS_FOLDER_NAME;
+    }
+    if (MISC_MEDIA_FLDER_NAME.equalsIgnoreCase(documentType)) {
+      return MISC_MEDIA_FLDER_NAME;
+    }
+    if (DMP_MEDIA_FLDER_NAME.equalsIgnoreCase(documentType)) {
+      return DMP_MEDIA_FLDER_NAME;
+    }
+    return DOCUMENT_MEDIA_FLDER_NAME;
   }
 }
