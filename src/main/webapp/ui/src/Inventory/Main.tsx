7943566c89b21305915a0da962dfc503f6932a75
Robert Lamacraft <robert.lamacraft@researchspace.com>
2025-09-10 14:14:19 +0100

    Fix background colour of search listing panel

--- src/main/webapp/ui/src/Inventory/Main.tsx
+++ src/main/webapp/ui/src/Inventory/Main.tsx
@@ -2,15 +2,12 @@ import { makeStyles } from "tss-react/mui";
 import * as React from "react";
 import clsx from "clsx";
 import styled from "@mui/system/styled";
-import { lighten } from "@mui/material";
 
 const useStyles = makeStyles()((theme) => ({
   main: {
     flexGrow: 1,
     minWidth: 0, // this is for the right hand panel 'title' ellipsis to work as expected
     height: "100%",
-    // backgroundColor:  `hsl(${theme.palette.primary.background.hue}deg, ${theme.palette.primary.background.saturation}%, 98%)`,
-    backgroundColor: lighten(theme.palette.primary.background, 0.98),
   },
   hiddenSidebar: {
     paddingTop: theme.spacing(7), // this for the header which includes the open sidebar button
