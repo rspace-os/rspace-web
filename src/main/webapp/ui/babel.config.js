77caef5c0186aec9eb597b20b33c35f95f4bd58c
Timothy Choi <timothy.choi@researchspace.com>
2025-10-22 19:18:28 +0200

    Update build tools defaults

--- src/main/webapp/ui/babel.config.js
+++ src/main/webapp/ui/babel.config.js
@@ -3,12 +3,10 @@ module.exports = {
     [
       "@babel/preset-env",
       {
-        targets: {
-          node: "current",
-        },
+        bugfixes: true,
       },
     ],
-    "@babel/preset-react",
+    ["@babel/preset-react", { runtime: "automatic" }],
   ],
   plugins: [
     [
