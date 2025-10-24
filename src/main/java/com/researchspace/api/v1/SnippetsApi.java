00771422fcedfce22b44c65e1573ed353622511d
rs-fraser <141634676+rs-fraser@users.noreply.github.com>
2025-10-24 11:38:35 +0100

    tidy endpoint path declaration

--- src/main/java/com/researchspace/api/v1/SnippetsApi.java
+++ src/main/java/com/researchspace/api/v1/SnippetsApi.java
@@ -5,11 +5,13 @@ import com.researchspace.model.User;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/api/v1/snippet/{id}")
 public interface SnippetsApi {
-  @GetMapping(value = "/api/v1/snippet/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+  @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
   ApiSnippet getSnippetById(@PathVariable long id, User user);
 
-  @GetMapping(value = "/api/v1/snippet/{id}/content", produces = MediaType.TEXT_PLAIN_VALUE)
+  @GetMapping(value = "/content", produces = MediaType.TEXT_PLAIN_VALUE)
   String getSnippetContentById(@PathVariable long id, User user);
 }
