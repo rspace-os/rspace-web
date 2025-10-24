9fc69edb630717dfa553c28d441642b0151c03c8
rs-fraser <141634676+rs-fraser@users.noreply.github.com>
2025-10-24 11:32:15 +0100

    remove unneccassery annotations

--- src/main/java/com/researchspace/api/v1/model/ApiSnippet.java
+++ src/main/java/com/researchspace/api/v1/model/ApiSnippet.java
@@ -1,6 +1,5 @@
 package com.researchspace.api.v1.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.researchspace.model.record.Snippet;
 import lombok.Data;
@@ -12,10 +11,8 @@ import lombok.NoArgsConstructor;
 @EqualsAndHashCode(callSuper = true)
 @JsonPropertyOrder({"id", "globalId", "name", "owner", "content", "_links"})
 public class ApiSnippet extends IdentifiableNameableApiObject {
-  @JsonProperty("owner")
   private ApiUser owner;
 
-  @JsonProperty("content")
   private String content;
 
   public ApiSnippet(Snippet snippet) {
