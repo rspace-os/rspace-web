c0e63efc9af1d4be0c6de34a0c629c99669891f6
nhanlon2 <neil.hanlon@hotmail.co.uk>
2025-12-04 12:41:38 +0000

    RSDEV-910-export-galaxy-data: correctly query for existing workflows

--- src/main/java/com/researchspace/dao/hibernate/ExternalWorkFlowDaoHibernate.java
+++ src/main/java/com/researchspace/dao/hibernate/ExternalWorkFlowDaoHibernate.java
@@ -3,6 +3,7 @@ package com.researchspace.dao.hibernate;
 import com.researchspace.dao.ExternalWorkFlowDao;
 import com.researchspace.dao.GenericDaoHibernate;
 import com.researchspace.model.externalWorkflows.ExternalWorkFlow;
+import java.util.List;
 import org.springframework.stereotype.Repository;
 
 @Repository("externalWorkFlowDao")
@@ -15,15 +16,20 @@ public class ExternalWorkFlowDaoHibernate extends GenericDaoHibernate<ExternalWo
 
   @Override
   public ExternalWorkFlow findWorkFlowByExtIdAndName(String extId, String name) {
-    return sessionFactory
-        .getCurrentSession()
-        .createQuery(
-            "from ExternalWorkFlow ewf join fetch ewf.externalWorkflowInvocations"
-                + " where ewf.extId ="
-                + " (:extId) and ewf.name = (:name) ",
-            ExternalWorkFlow.class)
-        .setParameter("extId", extId)
-        .setParameter("name", name)
-        .getSingleResult();
+    List<ExternalWorkFlow> wfList =
+        sessionFactory
+            .getCurrentSession()
+            .createQuery(
+                "from ExternalWorkFlow ewf join fetch ewf.externalWorkflowInvocations"
+                    + " where ewf.extId ="
+                    + " (:extId) and ewf.name = (:name) ",
+                ExternalWorkFlow.class)
+            .setParameter("extId", extId)
+            .setParameter("name", name)
+            .list();
+    if (wfList.isEmpty()) {
+      return null;
+    }
+    return wfList.get(0);
   }
 }
