baea0dee2f8242a5bcb42d5e7cd6385916e52af3
matthias <matthias@researchspace.com>
2026-01-14 14:03:23 +0000

    update unit tests

--- src/test/java/com/researchspace/api/v1/controller/InventoryFilesApiControllerMVCIT.java
+++ src/test/java/com/researchspace/api/v1/controller/InventoryFilesApiControllerMVCIT.java
@@ -158,7 +158,7 @@ public class InventoryFilesApiControllerMVCIT extends API_MVC_InventoryTestBase 
     String apiKey = createNewApiKeyForUser(user);
 
     // update sample with image
-    InputStream imageFile = RSpaceTestUtils.getInputStreamOnFromTestResourcesFolder("Picture1.png");
+    InputStream imageFile = RSpaceTestUtils.getInputStreamOnFromTestResourcesFolder("Picture1.base64");
     String imageBytes = Arrays.toString(imageFile.readAllBytes());
     sample.setNewBase64Image(imageBytes);
     sampleApiMgr.updateApiSample(sample, user);
@@ -183,7 +183,7 @@ public class InventoryFilesApiControllerMVCIT extends API_MVC_InventoryTestBase 
     ApiSampleWithFullSubSamples sample = createBasicSampleForUser(user1);
 
     // update sample with image
-    InputStream imageFile = RSpaceTestUtils.getInputStreamOnFromTestResourcesFolder("Picture1.png");
+    InputStream imageFile = RSpaceTestUtils.getInputStreamOnFromTestResourcesFolder("Picture1.base64");
     String imageBytes = Arrays.toString(imageFile.readAllBytes());
     sample.setNewBase64Image(imageBytes);
     sampleApiMgr.updateApiSample(sample, user1);
