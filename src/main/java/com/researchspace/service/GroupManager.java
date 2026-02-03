ae63afd65292f465981e5608cf8a532d31c0b8f4
Nico Ferrante <160590956+rs-nicof@users.noreply.github.com>
2026-02-03 18:08:43 +0000

    PRT-1039: Move GroupInfo creation into the manager to avoid Hibernate Session exception (#646)

--- src/main/java/com/researchspace/service/GroupManager.java
+++ src/main/java/com/researchspace/service/GroupManager.java
@@ -36,6 +36,12 @@ public interface GroupManager {
   /** Get a ApiGroupInfo by its id */
   Optional<ApiGroupInfo> getGroupInfoById(Long groupId);
 
+  /** Get a list of ApiGroupInfo where the user belongs */
+  List<ApiGroupInfo> getGroupInfoListByUser(User subject);
+
+  /** Get a list of ApiGroupInfo where the user belongs */
+  List<ApiGroupInfo> getGroupInfoListByQuery(User subject, PaginationCriteria<Group> pagCrit);
+
   /** Gets all the Groups */
   List<Group> list();
 
