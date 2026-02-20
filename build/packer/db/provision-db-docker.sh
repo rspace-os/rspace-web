ff21121372c9c6f5f4038b4e3353745654f7a1b1
matthias <matthias@researchspace.com>
2026-02-20 08:17:29 +0000

    remove outdated docker/packer setup within rspace-web project, and option in Jenkinsfile

--- build/packer/db/provision-db-docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-## Moving to this special folder will run the script 
-## on 1st execution.
-set -x
-mv rs-dbbaseline-utf8.sql docker-entrypoint-initdb.d/
--- build/packer/web/resources/setenv.sh
+++
@@ -1,2 +0,0 @@
-JAVA_OPTS="-Xms256m -Xmx1024m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/media/rspace/logs-audit"
-CATALINA_OPTS="-DpropertyFileDir=file:/etc/rspace/ -DRS_FILE_BASE=/media/rspace/file_store -Djava.awt.headless=true -Dliquibase.context=run -Dspring.profiles.active=prod -Djmelody.dir=/media/rspace/jmelody/"
