7d7839cf3965cde1565743880532d5672f6f0d98
Martin Rowe-Tan <30328804+mrowe-tan@users.noreply.github.com>
2026-03-03 10:40:47 +0000

    Resolving issue with failing unit tests. (#694)
    ## Description ##
    - Addresses failing unit tests as a result of changes made in
    https://github.com/rspace-os/rspace-web/pull/686

--- src/test/java/com/researchspace/webapp/integrations/dsw/DSWControllerRealConnectionTest.java
+++ src/test/java/com/researchspace/webapp/integrations/dsw/DSWControllerRealConnectionTest.java
@@ -117,7 +117,7 @@ public class DSWControllerRealConnectionTest extends SpringTransactionalTest {
     when(userAppConfigMgr.findByAppConfigElementSetId(null)).thenReturn(Optional.of(aces));
     when(source.findByUserNameProviderName(anyString(), anyString(), anyString()))
         .thenReturn(Optional.of(connection));
-    when(mediaManager.saveNewDMP(anyString(), any(), any(), any()))
+    when(mediaManager.saveNewDMPWithDescription(anyString(), any(), any(), any(), anyString()))
         .thenReturn(new EcatDocumentFile());
   }
 
@@ -206,7 +206,7 @@ public class DSWControllerRealConnectionTest extends SpringTransactionalTest {
   @RunIfSystemPropertyDefined("nightly")
   public void testImportPlanNullFileWhenSaving() {
     try {
-      when(mediaManager.saveNewDMP(anyString(), any(), any(), any()))
+      when(mediaManager.saveNewDMPWithDescription(anyString(), any(), any(), any(), anyString()))
           .thenReturn(null);
 
       AjaxReturnObject plansResponse = dswController.listDSWPlans(DSW_SERVER_ALIAS);
