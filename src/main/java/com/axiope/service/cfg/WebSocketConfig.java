21ddf281ca1309cbd5001f35034a4e52e5a5557d
rs-fraser <141634676+rs-fraser@users.noreply.github.com>
2026-05-14 13:34:14 +0100

    fallback websocket when running with jetty

--- src/main/java/com/axiope/service/cfg/WebSocketConfig.java
+++ src/main/java/com/axiope/service/cfg/WebSocketConfig.java
@@ -5,10 +5,9 @@ import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.SockJsServiceRegistration;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.server.jetty.JettyRequestUpgradeStrategy;
-import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -28,10 +27,22 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
     List<String> acceptedDomains = originRefererChecker.listAcceptedDomains();
-    registry
-        .addEndpoint("/ws")
-        .setHandshakeHandler(new DefaultHandshakeHandler(new JettyRequestUpgradeStrategy()))
-        .setAllowedOrigins(acceptedDomains.toArray(new String[0]))
-        .withSockJS();
+    SockJsServiceRegistration sockJs =
+        registry
+            .addEndpoint("/ws")
+            .setAllowedOrigins(acceptedDomains.toArray(new String[0]))
+            .withSockJS();
+    if (!isTomcatContainer()) {
+      sockJs.setWebSocketEnabled(false);
+    }
+  }
+
+  private boolean isTomcatContainer() {
+    try {
+      Class.forName("org.apache.tomcat.websocket.server.WsServerContainer");
+      return true;
+    } catch (ClassNotFoundException e) {
+      return false;
+    }
   }
 }
