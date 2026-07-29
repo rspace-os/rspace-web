a326b81f485b8e6ca39bb3e86ce25ea777ff4b2e
Timothy Choi <timothy.choi@researchspace.com>
2026-07-30 01:20:03 +0200

    Fix strict Mockito fixtures in Spring tests

--- src/test/java/com/researchspace/service/impl/SystemConfigurationInitializorTest.java
+++ src/test/java/com/researchspace/service/impl/SystemConfigurationInitializorTest.java
@@ -67,11 +67,7 @@ public class SystemConfigurationInitializorTest extends SpringTransactionalTest 
     systemConfigurationInitialisor.setChemistryProvider("indigo");
     systemConfigurationInitialisor.setChemistryServiceUrl("http://indigoService:8090");
 
-    // system setting is 'ALLOWED' and properties are configured - do nothing
-    when(mockSystemPropertyManager.findByName(SystemPropertyName.CHEMISTRY_AVAILABLE))
-        .thenReturn(
-            new SystemPropertyValue(
-                new SystemProperty(null), HierarchicalPermission.ALLOWED.name()));
+    // properties are configured, so the system setting is not consulted
     systemConfigurationInitialisor.onAppStartup(null);
     verify(mockSystemPropertyManager, Mockito.times(1))
         .save(Mockito.any(), (HierarchicalPermission) Mockito.any(), Mockito.any());
