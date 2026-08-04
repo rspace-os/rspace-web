1022465ebc434b71d3081b1e457d11bf8f2fe5d6
Timothy Choi <timothy.choi@researchspace.com>
2026-08-04 15:51:08 +0200

    Remove exception serial version identifiers

--- src/main/java/com/researchspace/maintenance/service/MaintenanceOperationException.java
+++ src/main/java/com/researchspace/maintenance/service/MaintenanceOperationException.java
@@ -3,8 +3,6 @@ package com.researchspace.maintenance.service;
 /** Domain failure while applying a maintenance collection mutation. */
 public class MaintenanceOperationException extends RuntimeException {
 
-  private static final long serialVersionUID = -8397253690055157362L;
-
   public enum Reason {
     INVALID_WINDOW
   }
