25c539c9c45ab2bd503d47559a256b3cd8b6b2b3
Timothy Choi <timothy.choi@researchspace.com>
2026-08-27 23:54:26 +0200

    Fix missing import

--- src/test/java/com/researchspace/service/impl/StoichiometryInventoryLinkManagerImplTest.java
+++ src/test/java/com/researchspace/service/impl/StoichiometryInventoryLinkManagerImplTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
