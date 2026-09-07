31f6b6e0c0e00d8c38fe60766523a616b3d75ee3
Timothy Choi <timothy.choi@researchspace.com>
2026-09-07 13:48:46 +0200

    feat(collections): add typed queries and permission-aware inventory adapters

--- src/main/java/com/researchspace/model/inventory/field/ExtraNumberField.java
+++ src/main/java/com/researchspace/model/inventory/field/ExtraNumberField.java
@@ -5,9 +5,6 @@ import com.researchspace.model.field.FieldType;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Transient;
-import java.text.MessageFormat;
-import java.util.Locale;
-import java.util.ResourceBundle;
 import lombok.EqualsAndHashCode;
 import org.apache.commons.lang3.StringUtils;
 import org.hibernate.envers.Audited;
@@ -20,9 +17,6 @@ public class ExtraNumberField extends ExtraField {
 
   private static final long serialVersionUID = 616794358851234028L;
   private static final String DEFAULT_NAME = "Numeric data";
-  public static final String INVALID_NUMBER_MESSAGE = "errors.inventory.field.numberInvalid";
-  private static final ResourceBundle VALIDATION_MESSAGES =
-      ResourceBundle.getBundle("ValidationMessages", Locale.ENGLISH);
 
   public ExtraNumberField() {
     setName(DEFAULT_NAME);
@@ -54,14 +48,10 @@ public class ExtraNumberField extends ExtraField {
       try {
         RuntimeFieldValueType.parseNumber(data);
       } catch (IllegalArgumentException nfe) {
-        return formatValidationMessage(INVALID_NUMBER_MESSAGE, data);
+        return "'" + data + "' cannot be parsed into number";
       }
     }
     return null;
-  }
-
-  private static String formatValidationMessage(String key, String value) {
-    return MessageFormat.format(VALIDATION_MESSAGES.getString(key), value);
   }
 
   @Override
