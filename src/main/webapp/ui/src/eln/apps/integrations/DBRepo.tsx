85ffea4c0b2335fd3b176855e7592aa0cb59a4b2
Akos Kukucska <a0sf3c@inf.elte.hu>
2026-09-23 17:16:21 +0200

    Use only one svg for the DBRepo integration, as requested by @rs-nicof

--- src/main/webapp/ui/src/eln/apps/integrations/DBRepo.tsx
+++ src/main/webapp/ui/src/eln/apps/integrations/DBRepo.tsx
@@ -8,7 +8,6 @@ import { useTranslation } from "react-i18next";
 import { useBroadcastChannel } from "@/modules/common/hooks/broadcast";
 import TransRichText from "@/modules/common/i18n/TransRichText";
 import { LOGO_COLOR } from "../../../assets/branding/dbrepo";
-import DBRepoIcon from "../../../assets/branding/dbrepo/logo.svg";
 import AlertContext, { mkAlert } from "../../../stores/contexts/Alert";
 import { Optional } from "../../../util/optional";
 import IntegrationCard from "../IntegrationCard";
@@ -26,6 +25,7 @@ export interface DBRepoConnectedMessage extends Record<string, unknown> {
 }
 
 export const DBREPO_CONNECTION_CHANNEL = "rspace.apps.dbrepo.connection";
+const DBREPO_ICON_PATH = "/images/icons/dbrepo.svg";
 
 function DBRepo({ integrationState, update }: DBRepoArgs): React.ReactNode {
   const { t } = useTranslation(["apps", "common"]);
@@ -70,7 +70,7 @@ function DBRepo({ integrationState, update }: DBRepoArgs): React.ReactNode {
         name={t("integrations.dbrepo.name")}
         integrationState={integrationState}
         explanatoryText={t("integrations.dbrepo.description")}
-        image={DBRepoIcon}
+        image={DBREPO_ICON_PATH}
         color={LOGO_COLOR}
         update={(newMode) =>
           update({
