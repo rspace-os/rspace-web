aa13f4cf0d4d1af37f9d9ce6ab24e0fe41d21caf
Kukucska Ákos <70856991+akos326@users.noreply.github.com>
2026-09-24 17:11:32 +0200

    Update src/main/java/com/researchspace/archive/model/ArchiveModelFactory.java
    Co-authored-by: Nico Ferrante <160590956+rs-nicof@users.noreply.github.com>

--- src/main/java/com/researchspace/archive/model/ArchiveModelFactory.java
+++ src/main/java/com/researchspace/archive/model/ArchiveModelFactory.java
@@ -116,7 +116,7 @@ public class ArchiveModelFactory {
         ChoiceFieldForm cfm = (ChoiceFieldForm) fm;
         rc.setOptions(cfm.getChoiceOptions());
         rc.setSelection(cfm.getDefaultChoiceOption());
-        rc.setMultipleChoice(cfm.isMultipleChoice() ? "yes" : "no"); // why din't cfm set.
+        rc.setMultipleChoice(cfm.isMultipleChoice() ? "yes" : "no");
         break;
       case DATE: // Date
         // <summary>Type: [DATE], Min date: [Unspecified], Max date:
